4a2658aa19d96eaef2153de898009f027ed884b3
knipferrc <knipferrc@gmail.com>
2016-10-16 10:54:16 -0500

    fixed issue with background color

--- src/views/App.js
+++ src/views/App.js
@@ -2,26 +2,16 @@ import React from 'react'
 import { StyleRoot } from 'radium';
 import Navigation from '../components/Navigation'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
-import { grey300 } from 'material-ui/styles/colors'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-
-const styles = {
-  hero: {
-    backgroundColor: grey300,
-    height: "100vh"
-  }
-}
 
 function App({children}) {
   return (
-    <div style={styles.hero}>
-      <MuiThemeProvider muiTheme={getMuiTheme()}>
-        <StyleRoot>
-          <Navigation />
-          {children}
-        </StyleRoot>
-      </MuiThemeProvider>
-    </div>
+    <MuiThemeProvider muiTheme={getMuiTheme()}>
+      <StyleRoot>
+        <Navigation />
+        {children}
+      </StyleRoot>
+    </MuiThemeProvider>
   )
 }
 
