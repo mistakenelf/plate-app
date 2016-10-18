e130d72db7b9083621b0f1bd92d34a6f0d2db3b0
knipferrc <knipferrc@gmail.com>
2016-10-17 20:13:26 -0500

    added firebase config

--- src/index.js
+++ src/index.js
@@ -7,6 +7,17 @@ import Login from './views/login/Login'
 import Register from './views/register/Register'
 import NotFound from './components/NotFound'
 import './index.css'
+import * as firebase from 'firebase'
+
+const config = {
+  apiKey: "AIzaSyDjNxAdCF1uaRzZSlHxw8CDyYTnfb83c5M",
+  authDomain: "spendy-8eb10.firebaseapp.com",
+  databaseURL: "https://spendy-8eb10.firebaseio.com",
+  storageBucket: "spendy-8eb10.appspot.com",
+  messagingSenderId: "500637043981"
+};
+
+firebase.initializeApp(config);
 
 render(
   <Router history={hashHistory}>
