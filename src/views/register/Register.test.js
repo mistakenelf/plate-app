7409c0fd9c98feab8725f68aa2a1e5fff9102fc5
knipferrc <knipferrc@gmail.com>
2016-10-21 16:44:23 -0500

    fix tests

--- src/views/register/Register.test.js
+++ src/views/register/Register.test.js
@@ -5,6 +5,18 @@ import injectTapEventPlugin from 'react-tap-event-plugin'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import Register from './Register'
+import * as firebase from 'firebase'
+
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyDL-jDWSC5lrRfRsngBu2gPTEmL2hO2SXE",
+  authDomain: "plate-a0b1b.firebaseapp.com",
+  databaseURL: "https://plate-a0b1b.firebaseio.com",
+  storageBucket: "plate-a0b1b.appspot.com",
+  messagingSenderId: "1011831735527"
+}
+
+firebase.initializeApp(config)
 
 injectTapEventPlugin()
 
