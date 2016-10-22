4569deebbdd0e61fd2bb3516ef57dc5acb40a034
Alex Knipfer <cr33pyxjtag@yahoo.com>
2016-10-21 21:01:31 -0500

    added text for dashboard menu header and textfield

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Grid, Cell } from 'radium-grid'
-import { blue800 } from 'material-ui/styles/colors'
-import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
-import FlatButton from 'material-ui/FlatButton';
+import { cyan500 } from 'material-ui/styles/colors'
+import TextField from 'material-ui/TextField';
 
 const styles = {
   menuLayout: {
-    backgroundColor: blue800,
+    backgroundColor: cyan500,
     height: 200,
     width: '100%'
+  },
+
+  menuHeader: {
+    color: 'white'
   }
 }
 
@@ -16,24 +19,15 @@ function Dashboard() {
   return (
     <Grid>
       <Cell width="1">
-        <Card style={styles.menuLayout}>
-          <CardHeader
-            title="Without Avatar"
-            subtitle="Subtitle"
-            actAsExpander={true}
-            showExpandableButton={true}
-          />
-          <CardActions>
-            <FlatButton label="Action1" />
-            <FlatButton label="Action2" />
-          </CardActions>
-          <CardText expandable={true}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-          </CardText>
-        </Card>
+        <div style={styles.menuLayout}>
+          <Cell width="1/4"></Cell>
+          <Cell width="1/2" style={{ justifyContent: 'center' }}>
+            <h2 style={styles.menuHeader}>create a group</h2>
+            <TextField
+              hintText="Hint Text"
+            />
+          </Cell>
+        </div>
       </Cell>
     </Grid>
   )
