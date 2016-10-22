aab61a2a23abd49644cf3cdcc6357ef9aad3d260
Alex Knipfer <cr33pyxjtag@yahoo.com>
2016-10-22 18:58:37 -0500

    fix conflicts

---
+++ src/components/notFound/NotFound.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render } from 'react-dom'
+import NotFound from './NotFound'
+
+it('renders without crashing', () => {
+  const div = document.createElement('div')
+  render(
+    <NotFound/>,
+  div)
+})
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from 'react-dom'
+import AppContainer from './containers/appContainer/AppContainer'
+import './index.css'
+
+render(
+  <AppContainer />,
+  document.getElementById('root')
+)
