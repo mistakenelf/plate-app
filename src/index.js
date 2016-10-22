740452936e2863afbb6748214ddb5b6472c8c80f
Alex Knipfer <cr33pyxjtag@yahoo.com>
2016-10-22 18:49:18 -0500

    removed src

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render } from 'react-dom'
-import AppContainer from './containers/appContainer/AppContainer'
-import './index.css'
-
-render(
-  <AppContainer />,
-  document.getElementById('root')
-)
