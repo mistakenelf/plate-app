dd81d2f3dddeaf626dd0580c0df427a8e4fcfbaa
Alex Knipfer <cr33pyxjtag@yahoo.com>
2016-10-22 21:15:13 -0500

    changed nav color

--- src/components/navigation/UnauthenticatedNav.js
+++ src/components/navigation/UnauthenticatedNav.js
@@ -5,13 +5,12 @@ import IconButton from 'material-ui/IconButton'
 import IconMenu from 'material-ui/IconMenu'
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert'
 import MenuItem from 'material-ui/MenuItem'
-import { indigo500 } from 'material-ui/styles/colors'
 import { IndexLink, Link } from 'react-router'
 import { StyleSheet, css } from 'aphrodite'
 
 const styles = StyleSheet.create({
   appBarColor: {
-    backgroundColor: indigo500,
+    backgroundColor: '#6147A4',
   }
 })
 
