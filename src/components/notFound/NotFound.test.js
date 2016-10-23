d0ef4cdcd9d0b0642d5e91516a2200737f99601e
knipferrc <knipferrc@gmail.com>
2016-10-23 13:39:14 -0500

    fix test

--- src/components/notFound/NotFound.test.js
+++ src/components/notFound/NotFound.test.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import { render } from 'react-dom'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import NotFound from './NotFound'
+
+injectTapEventPlugin()
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
   render(
-    <NotFound/>,
+    <MuiThemeProvider muiTheme={getMuiTheme()}>
+      <NotFound />
+    </MuiThemeProvider>,
   div)
 })
