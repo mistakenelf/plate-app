43faa21c8876b0d87e4240d174cbcc77438f8231
knipferrc <knipferrc@gmail.com>
2016-11-12 09:09:55 -0600

    move routes to its own file

--- src/containers/app/App.js
+++ src/containers/app/App.js
@@ -1,17 +1,10 @@
 import React from 'react'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { Router, browserHistory } from 'react-router'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 import * as firebase from 'firebase'
-import MainLayout from '../../layouts/MainLayout'
-import FormLayout from '../../layouts/FormLayout'
-import Home from '../../pages/home/Home'
-import Login from '../../pages/login/Login'
-import Register from '../../pages/register/Register'
-import NotFound from '../../components/notFound/NotFound'
-import Dashboard from '../../pages/dashboard/Dashboard'
-import Workspace from '../../pages/workspace/Workspace'
+import routes from '../../routes/routes'
 import 'bootstrap/dist/css/bootstrap.css'
 
 injectTapEventPlugin()
@@ -30,22 +23,7 @@ firebase.initializeApp(config)
 function App() {
   return (
     <MuiThemeProvider muiTheme={getMuiTheme()}>
-      <Router history={browserHistory}>
-        <Route path="/">
-          <Route component={FormLayout}>
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-          </Route>
-          <Route component={MainLayout}>
-            <IndexRoute component={Home} />
-            <Route path="/dashboard" component={Dashboard} />
-            <Route path="/workspace" component={Workspace} />
-          </Route>
-          <Route component={FormLayout}>
-            <Route path="*" component={NotFound} />
-          </Route>
-        </Route>
-      </Router>
+      <Router history={browserHistory} routes={routes} />
     </MuiThemeProvider>
   )
 }
---
+++ src/routes/routes.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { IndexRoute, Route } from 'react-router'
+import MainLayout from '../layouts/MainLayout'
+import FormLayout from '../layouts/FormLayout'
+import Home from '../pages/home/Home'
+import Login from '../pages/login/Login'
+import Register from '../pages/register/Register'
+import NotFound from '../components/notFound/NotFound'
+import Dashboard from '../pages/dashboard/Dashboard'
+import Workspace from '../pages/workspace/Workspace'
+
+const routes = (
+  <Route path="/">
+    <Route component={FormLayout}>
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
+    </Route>
+    <Route component={MainLayout}>
+      <IndexRoute component={Home} />
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/workspace" component={Workspace} />
+    </Route>
+    <Route component={FormLayout}>
+      <Route path="*" component={NotFound} />
+    </Route>
+  </Route>
+)
+
+export default routes
