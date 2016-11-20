5a4b1310ed7645c49d68d6af74245e51a1500aa2
knipferrc <knipferrc@gmail.com>
2016-11-19 19:44:41 -0600

    new layout

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,14 +1,32 @@
-import React from 'react'
-import { Grid, Row } from 'react-bootstrap'
-import AccountCard from './AccountCard'
-import TasksCard from './TasksCard'
+import { Col, Grid, Row } from 'react-bootstrap'
 
-function Dashboard({tasks}) {
+import AccountCard from './AccountCard'
+import Divider from 'material-ui/Divider'
+import InfoCard from './InfoCard'
+import React from 'react'
+import TaskCard from './TaskCard'
+import TaskCreator from './TaskCreator'
+
+function Dashboard({ tasks }) {
   return (
     <Grid fluid>
       <Row>
-        <TasksCard tasks={tasks}/>
+        <TaskCreator tasks={tasks} />
         <AccountCard />
+        <InfoCard />
+      </Row>
+      <br />
+      <Row>
+        <Col sm={12} lg={12} md={12}>
+          <Divider />
+        </Col>
+      </Row>
+      <Row>
+        {tasks.map((task, index) => {
+          return (
+            <TaskCard key={index} task={task} />
+          )
+        })}
       </Row>
     </Grid>
   )
