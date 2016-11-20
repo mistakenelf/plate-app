553c253f84eff677bbed6d5809d0870c04bac55d
knipferrc <knipferrc@gmail.com>
2016-11-19 19:45:26 -0600

    added forgot password

---
+++ src/pages/forgotPassword/ForgotPassword.js
@@ -0,0 +1,53 @@
+import * as firebase from 'firebase'
+
+import { Card, CardText, CardTitle } from 'material-ui/Card'
+import { Col, Grid, Row } from 'react-bootstrap'
+import React, { Component } from 'react'
+
+import Alert from 'react-s-alert'
+import TextField from 'material-ui/TextField'
+import { browserHistory } from 'react-router'
+import { css } from 'aphrodite'
+import styles from './styles'
+
+export default class ForgotPassword extends Component {
+  forgotPassword(e) {
+    e.preventDefault()
+
+    const email = document.getElementById('email').value
+
+    const promise = firebase.auth().sendPasswordResetEmail(email)
+
+    promise
+      .then(() => browserHistory.push('/'))
+      .then(() => 
+        Alert.success('Email has been sent', {
+            position: 'top-right',
+        })
+      )
+      .catch((e) => 
+        Alert.error(e.message, {
+          position: 'top-right'
+        })
+      )
+  }
+
+  render() {
+    return (
+      <Grid fluid>
+        <Row>
+          <Col xs={12} sm={8} smOffset={2} md={6} mdOffset={3} lg={4} lgOffset={4}>
+            <Card className={css(styles.loginMargin)}>
+              <CardTitle title="Forgot Password" subtitle="Please enter your email" />
+              <CardText>
+                <form onSubmit={this.forgotPassword}>
+                  <TextField hintText="Email" id="email" fullWidth={true} required={true} />
+                </form>
+              </CardText>
+            </Card>
+          </Col>
+        </Row>
+      </Grid>
+    )
+  }
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,19 +1,20 @@
-import React from 'react'
 import { IndexRoute, Route } from 'react-router'
-import MainLayout from '../layouts/MainLayout'
+
+import DashboardContainer from '../containers/DashboardContainer'
+import ForgotPassword from '../pages/forgotPassword/ForgotPassword'
 import Home from '../pages/home/Home'
 import Login from '../pages/login/Login'
-import Register from '../pages/register/Register'
+import MainLayout from '../layouts/MainLayout'
 import NotFound from '../components/notFound/NotFound'
-import DashboardContainer from '../containers/DashboardContainer'
-import Workspace from '../pages/workspace/Workspace'
+import React from 'react'
+import Register from '../pages/register/Register'
 
 const routes = (
   <Route path="/">
     <Route component={MainLayout}>
       <IndexRoute component={Home} />
       <Route path="/dashboard" component={DashboardContainer} />
-      <Route path="/workspace" component={Workspace} />
+      <Route path="/forgotPassword" component={ForgotPassword} />
       <Route path="/login" component={Login} />
       <Route path="/register" component={Register} />
       <Route path="*" component={NotFound} />
