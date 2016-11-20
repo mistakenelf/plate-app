c829bd1b0e4f3447d7419df3f3e91f16bfcce367
knipferrc <knipferrc@gmail.com>
2016-11-19 19:45:34 -0600

    added description

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,11 @@
-import { createStore } from 'redux'
 import { browserHistory } from 'react-router'
-import { syncHistoryWithStore } from 'react-router-redux'
+import { createStore } from 'redux'
 import rootReducer from '../reducers'
+import { syncHistoryWithStore } from 'react-router-redux'
 
 const tasks = [
-  { "id": 1, "taskName": "Get redux setup" },
-  { "id": 2, "taskName": "Start using firebase" }
+  { "id": 1, "taskName": "Get redux setup", "description": "Use redux so we have one large store of data that we can use throughout the app" },
+  { "id": 2, "taskName": "Start using firebase", "description": "Start using firebase in this project so we can utilize some database features" }
 ]
 
 const defaultState = {
