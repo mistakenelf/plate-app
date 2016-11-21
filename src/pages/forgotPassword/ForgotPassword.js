309e625fe1546b9fc3c18fb4a85b4a9703befc4a
knipferrc <knipferrc@gmail.com>
2016-11-20 19:24:39 -0600

    new forgot password component

--- src/pages/forgotPassword/ForgotPassword.js
+++ src/pages/forgotPassword/ForgotPassword.js
@@ -1,14 +1,11 @@
 import * as firebase from 'firebase'
 
-import { Card, CardText, CardTitle } from 'material-ui/Card'
-import { Col, Grid, Row } from 'react-bootstrap'
+import { Button, Col, ControlLabel, FormControl, FormGroup, Grid, Panel, Row } from 'react-bootstrap'
 import React, { Component } from 'react'
 
-import RaisedButton from 'material-ui/RaisedButton'
-import TextField from 'material-ui/TextField'
+import { StyleSheet } from 'aphrodite'
 import { browserHistory } from 'react-router'
 import { css } from 'aphrodite'
-import styles from './styles'
 import { toastr } from 'react-redux-toastr'
 
 export default class ForgotPassword extends Component {
@@ -20,7 +17,7 @@ export default class ForgotPassword extends Component {
     const promise = firebase.auth().sendPasswordResetEmail(email)
 
     promise
-      .then(() => browserHistory.push('/'))
+      .then(() => browserHistory.push('/login'))
       .then(() =>
         toastr.success('Success', 'Reset password email has been sent')
       )
@@ -34,19 +31,26 @@ export default class ForgotPassword extends Component {
       <Grid fluid>
         <Row>
           <Col xs={12} sm={8} smOffset={2} md={6} mdOffset={3} lg={4} lgOffset={4}>
-            <Card className={css(styles.forgotFormMargin)}>
-              <CardTitle title="Forgot Password" subtitle="Please enter your email" />
-              <CardText>
-                <form onSubmit={this.forgotPassword}>
-                  <TextField hintText="Email" id="email" fullWidth={true} required={true} />
-                  <br />
-                  <RaisedButton type="submit" label="Forgot Password" secondary={true} fullWidth={true} />
-                </form>
-              </CardText>
-            </Card>
+            <Panel header={<h2>Forgot Password</h2>} bsStyle="primary" className={css(styles.forgotPasswordMargin)}>
+              <form onSubmit={this.forgotPassword}>
+                <FormGroup>
+                  <ControlLabel>Email:</ControlLabel>
+                  <FormControl type="email" id="email" placeholder="Email" required />
+                </FormGroup>
+                <FormGroup>
+                  <Button type="submit" bsStyle="primary" bsSize="large" block>Forgot Password</Button>
+                </FormGroup>
+              </form>
+            </Panel>
           </Col>
         </Row>
       </Grid>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  forgotPasswordMargin: {
+    marginTop: '25%'
+  }
+})
--- src/pages/forgotPassword/styles.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'aphrodite'
-
-const styles = StyleSheet.create({
-  forgotFormMargin: {
-    marginTop: 100
-  }
-})
-
-export default styles
