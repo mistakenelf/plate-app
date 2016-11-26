0d9aa9f009373c02397329123fd950450d6d6286
knipferrc <knipferrc@gmail.com>
2016-11-25 20:01:45 -0600

    enable redux devtools

--- src/store/store.js
+++ src/store/store.js
@@ -9,7 +9,9 @@ const defaultState = {
   plates
 }
 
-const store = createStore(rootReducer, defaultState)
+const store = createStore(rootReducer, defaultState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 export const history = syncHistoryWithStore(browserHistory, store)
 
