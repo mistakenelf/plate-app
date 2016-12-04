d26e2922ee2fa77c9f6774c2baa84ca78245c56a
knipferrc <knipferrc@gmail.com>
2016-12-04 11:41:59 -0600

    remove rebass styling

--- src/components/notFound/NotFound.js
+++ src/components/notFound/NotFound.js
@@ -5,23 +5,7 @@ import React from 'react'
 
 function NotFound() {
   return (
-    <Flex justify='center'>
-      <Box p={2} sm={12} md={6} lg={4}>
-        <Panel theme='info'>
-          <PanelHeader
-            inverted
-            theme='error'
-            >
-            Page Not Found
-          </PanelHeader>
-          <Text>
-            The page you are looking for has either been moved, or does
-            not exist anymore. Please contact your administrator if you
-            think this is an error. Thanks, Plate Team.
-          </Text>
-        </Panel>
-      </Box>
-    </Flex>
+    <h1>Not Found</h1>
   )
 }
 
