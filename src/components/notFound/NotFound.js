d29b9972e26daf4c667a70fe553d61cee77ae6b9
knipferrc <knipferrc@gmail.com>
2016-12-04 16:33:51 -0600

    new not found page

--- src/components/notFound/NotFound.js
+++ src/components/notFound/NotFound.js
@@ -1,8 +1,25 @@
+import { Card, Grid, Icon } from 'semantic-ui-react'
+
 import React from 'react'
 
 function NotFound() {
   return (
-    <h1>Not Found</h1>
+    <Grid centered padded>
+      <Grid.Column computer={8} largeScreen={4} tablet={10} mobile={16}>
+        <Card style={{ width: '100%', textAlign: 'center' }}>
+          <div style={{ textAlign: 'center'}}>
+            <Icon name='warning' size='massive' color='red' />
+          </div>
+          <Card.Content extra>
+            <p>
+              Sorry, but the page you are trying to reach does not exist.
+              If you believe this is an error, please contact your administrator
+              or your Plate team. Thanks!
+            </p>
+          </Card.Content>
+        </Card>
+      </Grid.Column>
+    </Grid>
   )
 }
 
