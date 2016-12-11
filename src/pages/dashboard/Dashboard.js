dc0f6e5a3cd9421e79c2c7d21ebcca62eed6fa60
knipferrc <knipferrc@gmail.com>
2016-12-11 09:32:15 -0600

    cleanup

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -4,22 +4,37 @@ import React, { PropTypes } from 'react'
 import Plate from './Plate'
 
 const propTypes = {
-  plates: PropTypes.array
+  plates: PropTypes.array,
+  addPlate: PropTypes.func,
+  removePlate: PropTypes.func
 }
 
-function Dashboard({ plates, ...otherProps }) {
+function Dashboard({ plates, addPlate, removePlate }) {
   return (
     <Grid padded>
       <Grid.Row>
         <Grid.Column>
-          <Button fluid color='pink' onClick={() => otherProps.addPlate()}>Add New Plate</Button>
+          <Button
+            fluid
+            color='pink'
+            onClick={() => addPlate()}
+          >
+            Add New Plate
+          </Button>
         </Grid.Column>
       </Grid.Row>
       <Grid.Row>
         {plates.map((plate, index) => {
           return (
-            <Grid.Column key={index} computer={4} largeScreen={4} tablet={8} mobile={16} style={{ paddingBottom: 10 }}>
-              <Plate key={index} plate={plate} {...otherProps} />
+            <Grid.Column
+              key={index}
+              computer={4}
+              largeScreen={4}
+              tablet={8}
+              mobile={16}
+              style={{ paddingBottom: 10 }}
+            >
+              <Plate key={index} plate={plate} removePlate={removePlate} />
             </Grid.Column>
           )
         })}
