89ca2f2ca092c17356e62cd3de9b61c64534ec1f
knipferrc <knipferrc@gmail.com>
2016-12-26 20:13:41 -0600

    get firebase ready

--- src/index.js
+++ src/index.js
@@ -1,27 +1,12 @@
 import './index.css'
 
-import * as firebase from 'firebase'
-
 import { Router, browserHistory } from 'react-router'
 
 import React from 'react'
 import { render } from 'react-dom'
 import routes from './routes/routes'
 
-// Initialize Firebase
-const config = {
-  apiKey: 'AIzaSyDL-jDWSC5lrRfRsngBu2gPTEmL2hO2SXE',
-  authDomain: 'plate-a0b1b.firebaseapp.com',
-  databaseURL: 'https://plate-a0b1b.firebaseio.com',
-  storageBucket: 'plate-a0b1b.appspot.com',
-  messagingSenderId: '1011831735527'
-}
-
-firebase.initializeApp(config)
-
 render(
-  <div>
-    <Router history={browserHistory} routes={routes} />
-  </div>,
+  <Router history={browserHistory} routes={routes} />,
   document.getElementById('root')
 )
