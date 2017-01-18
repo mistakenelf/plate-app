610d5366051a08c1097de398863dc3ae55800e83
Tyler Knipfer <knipferrc@gmail.com>
2017-01-18 10:12:09 -0600

    Update Layout.js

--- modules/layout/Layout.js
+++ modules/layout/Layout.js
@@ -34,7 +34,7 @@ const MainLayout = observer(class MainLayout extends Component {
           <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" />
           <meta name="theme-color" content="#00B5AD" />
           <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-          <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.6/semantic.min.css"></link>
+          <link rel="stylesheet" href="https//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.6/semantic.min.css"></link>
           <link rel="shortcut icon" href="/static/plateLogo.png" />
         </Head>
         <Sidebar
