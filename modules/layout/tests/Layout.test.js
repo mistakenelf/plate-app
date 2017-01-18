5af14ea016990b821c179bdaaf27ae29e0b51590
knipferrc <knipferrc@gmail.com>
2017-01-17 19:36:19 -0600

    fix tests

--- modules/layout/tests/Layout.test.js
+++ modules/layout/tests/Layout.test.js
@@ -1,12 +1,10 @@
-import MainLayout from '../MainLayout'
-import React from 'react'
-import TestWrapper from '../../testWrapper/TestWrapper'
+import Layout from '../Layout'
 import { render } from 'react-dom'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
   render(
-    <MainLayout />,
+    <Layout />,
     div
   )
 })
