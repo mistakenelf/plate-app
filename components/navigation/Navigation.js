4f90fb71d64d1ae37b9037c6775a10452d5bda96
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-01-24 12:34:56 -0600

    show unauthenticated nav

--- components/navigation/Navigation.js
+++ components/navigation/Navigation.js
@@ -1,35 +1,6 @@
-import React, { Component } from 'react'
+import React from 'react'
+import UnauthenticatedNav from './components/UnauthenticatedNav'
 
-import AppBar from 'material-ui/AppBar'
-import Drawer from 'material-ui/Drawer'
-import IconButton from 'material-ui/IconButton'
-import MenuItem from 'material-ui/MenuItem'
-import { observer } from 'mobx-react'
-import store from './store/store'
-
-function activateDrawer() {
-  store.menuActive = true
-}
-
-function closeDrawer() {
-  store.menuActive = false
-}
-
-export default observer(() =>
-  <div>
-    <AppBar
-      title='Plate'
-      onLeftIconButtonTouchTap={activateDrawer}
-      iconClassNameRight='muidocs-icon-navigation-expand-more'
-    />
-    <Drawer
-      open={store.menuActive}
-      onRequestChange={closeDrawer}
-      docked={false}
-      width={200}
-    >
-      <MenuItem>Register</MenuItem>
-      <MenuItem>Login</MenuItem>
-    </Drawer>
-  </div>
+export default () => (
+  <UnauthenticatedNav />
 )
