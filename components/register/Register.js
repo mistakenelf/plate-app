5c1835fd61384cb54391f4209279baf7c760f58b
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-01-24 14:37:43 -0600

    register page added

---
+++ components/register/Register.js
@@ -0,0 +1,3 @@
+export default () => (
+  <h3>Register</h3>
+)
---
+++ pages/register.js
@@ -0,0 +1,8 @@
+import Layout from '../components/layout/Layout'
+import Register from '../components/register/Register'
+
+export default () => (
+  <Layout>
+    <Register />
+  </Layout>
+)
