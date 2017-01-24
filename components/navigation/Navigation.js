a51223cbce9eb3d9d8490e6f9e625e4215efbeea
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-01-24 00:16:29 -0600

    added drawer

--- components/navigation/Navigation.js
+++ components/navigation/Navigation.js
@@ -1,8 +1,38 @@
+import React, { Component } from 'react'
+import { observer } from 'mobx-react'
 import AppBar from 'material-ui/AppBar'
+import MenuItem from 'material-ui/MenuItem'
+import Drawer from 'material-ui/Drawer'
+import IconButton from 'material-ui/IconButton'
+import store from './store/store'
 
-export default () => (
-  <AppBar
-    title="Plate"
-    iconClassNameRight="muidocs-icon-navigation-expand-more"
-  />
-)
+const Navigation = observer(class Navigation extends Component {
+
+  activateDrawer = () => {
+    store.menuActive = true
+  }
+
+  render() {
+    return (
+      <div>
+        <AppBar
+          title="Plate"
+          onRightIconButtonTouchTap={this.activateDrawer}
+          iconClassNameRight="muidocs-icon-navigation-expand-more"
+        />
+        <Drawer open={store.menuActive}>
+          <MenuItem>Register</MenuItem>
+          <MenuItem>Login</MenuItem>
+        </Drawer>
+      </div>
+    )
+  }
+
+})
+
+export default Navigation
+
+
+
+
+
