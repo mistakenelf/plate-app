6dcc4b877557b2925257d79b1434d00395304abc
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-01-24 17:49:19 -0600

    added layout

--- pages/register.js
+++ pages/register.js
@@ -1,8 +1,20 @@
+import { Component } from 'react'
+import Head from 'next/head'
 import Layout from '../modules/layout/Layout'
 import Register from '../modules/register/Register'
 
-export default () => (
-  <Layout>
-    <Register />
-  </Layout>
-)
+export default class extends Component {
+  static async getInitialProps({ req }) {
+    return req
+      ? { userAgent: req.headers['user-agent'] }
+      : { userAgent: navigator.userAgent }
+  }
+
+  render() {
+    return (
+      <Layout userAgent={this.props.userAgent}>
+        <Register />
+      </Layout>
+    )
+  }
+}
