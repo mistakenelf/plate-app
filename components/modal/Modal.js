64cea865350fefdaf00c0cfca5b89092d72e14fb
knipferrc <knipferrc@gmail.com>
2017-01-25 06:58:31 -0600

    modal fixes

--- components/modal/Modal.js
+++ components/modal/Modal.js
@@ -1,16 +1,16 @@
-export default ({ open }) => (
+export default ({ open, closeModal }) => (
   <div>
     <style jsx>{`
       .modal {
         height: 100vh;
         width: 100%;
         background-color: black;
-        z-index: 9999 !important;
-        position: relative;
+        z-index: 9999;
+        position: absolute;
       }
     `}</style>
-    {open 
-     ? <div className="modal"  />
+    {open
+     ? <div className="modal" onClick={closeModal}  />
      : <div className="modal" style={{display: 'none'}}  />
     }
   </div>
--- modules/navigation/components/UnauthenticatedNav.js
+++ modules/navigation/components/UnauthenticatedNav.js
@@ -2,14 +2,14 @@ import AppBar from 'material-ui/AppBar'
 import { Component } from 'react'
 import Drawer from 'material-ui/Drawer'
 import IconButton from 'material-ui/IconButton'
-import MenuItem from 'material-ui/MenuItem'
-import { observer } from 'mobx-react'
 import Link from 'next/prefetch'
+import MenuItem from 'material-ui/MenuItem'
 import Modal from '../../../components/modal/Modal'
+import { observer } from 'mobx-react'
 
 export default observer(({ store }) =>
   <div>
-    <style jsx>{` 
+    <style jsx>{`
       a {
         text-decoration: none;
       }
@@ -20,12 +20,12 @@ export default observer(({ store }) =>
       onLeftIconButtonTouchTap={store.openModal}
       style={{display: 'none'}}
     />
-    : <AppBar 
+    : <AppBar
       title='Plate'
       onLeftIconButtonTouchTap={store.openModal}
       style={{backgroundColor: 'white'}}
     />
     }
-    <Modal open={store.modalOpen} />
+    <Modal open={store.modalOpen} closeModal={store.closeModal} />
   </div>
 )
