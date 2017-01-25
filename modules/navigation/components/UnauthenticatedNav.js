d814e9ccc896f036a0ab0f00c90d180e9de21333
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-01-24 19:36:50 -0600

    appbar display modal

--- modules/navigation/components/UnauthenticatedNav.js
+++ modules/navigation/components/UnauthenticatedNav.js
@@ -5,27 +5,27 @@ import IconButton from 'material-ui/IconButton'
 import MenuItem from 'material-ui/MenuItem'
 import { observer } from 'mobx-react'
 import Link from 'next/prefetch'
+import Modal from '../../../components/modal/Modal'
 
 export default observer(({ store }) =>
   <div>
-    <style jsx>{`
+    <style jsx>{` 
       a {
         text-decoration: none;
       }
     `}</style>
-    <AppBar
+    {store.modalOpen
+    ? <AppBar
       title='Plate'
-      onLeftIconButtonTouchTap={store.openDrawer}
-      iconClassNameRight='muidocs-icon-navigation-expand-more'
+      onLeftIconButtonTouchTap={store.openModal}
+      style={{display: 'none'}}
     />
-    <Drawer
-      open={store.menuActive}
-      onRequestChange={store.closeDrawer}
-      docked={false}
-      width={200}
-    >
-      <Link href='/register'><a><MenuItem onClick={store.closeDrawer}>Register</MenuItem></a></Link>
-      <MenuItem>Login</MenuItem>
-    </Drawer>
+    : <AppBar 
+      title='Plate'
+      onLeftIconButtonTouchTap={store.openModal}
+      style={{backgroundColor: 'white'}}
+    />
+    }
+    <Modal open={store.modalOpen} />
   </div>
 )
