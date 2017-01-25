73f55c4c258eec9cf8c6f11918bcab90bc7eef0d
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-01-24 19:37:07 -0600

    useless import

--- pages/register.js
+++ pages/register.js
@@ -1,6 +1,6 @@
 import { Component } from 'react'
 import Head from 'next/head'
-import Layout from '../modules/layout/Layout'
+import Layout from '../components/layout/Layout'
 import Register from '../modules/register/Register'
 
 export default class extends Component {
