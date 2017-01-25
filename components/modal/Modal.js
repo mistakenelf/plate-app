cb9eb4b01e6e37093a8377edef7a2be054888b9a
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-01-25 00:03:04 -0600

    add z-index and relative positions

--- components/modal/Modal.js
+++ components/modal/Modal.js
@@ -2,10 +2,11 @@ export default ({ open }) => (
   <div>
     <style jsx>{`
       .modal {
-        opacity: 0.2;
         height: 100vh;
         width: 100%;
         background-color: black;
+        z-index: 9999 !important;
+        position: relative;
       }
     `}</style>
     {open 
