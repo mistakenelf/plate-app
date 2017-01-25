c9f2a8b48e26c9ef049d7b9cefc65b46c6615db3
knipferrc <knipferrc@gmail.com>
2017-01-25 15:44:39 -0600

    modal styling

--- components/modal/Modal.js
+++ components/modal/Modal.js
@@ -3,6 +3,8 @@ export default ({ open, closeModal }) => (
     {open
      ? <div className="modal">
           <h1 style={{color: 'white', textAlign: 'center'}} onTouchTap={closeModal}>Close</h1>
+          <h1 style={{color: 'white', textAlign: 'center'}}>Login</h1>
+          <h1 style={{color: 'white', textAlign: 'center'}}>Register</h1>
        </div>
      : <div className="modal" style={{display: 'none'}} />
     }
@@ -10,9 +12,16 @@ export default ({ open, closeModal }) => (
       .modal {
         height: 100vh;
         width: 100%;
+        padding-top: 110px;
+        padding-bottom: 110px;
+        opacity: 0.8;
         background-color: black;
         z-index: 9999;
         position: absolute;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        flex-direction: column;
       }
     `}</style>
   </div>
