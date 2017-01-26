28db574971450cee89c3a91e9025dcdb30b2f04a
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-01-25 23:24:23 -0600

    added fade in animation to modal

--- components/modal/Modal.js
+++ components/modal/Modal.js
@@ -3,7 +3,7 @@ import { inject, observer } from 'mobx-react'
 export default inject('store')(observer(({ store }) => (
   <div>
     {store.modalOpen
-     ? <div className="modal">
+     ? <div className="modal fadeIn">
           <h1 style={{color: 'white', textAlign: 'center'}} onTouchTap={store.closeModal}>Close</h1>
           <h1 style={{color: 'white', textAlign: 'center'}}>Login</h1>
           <h1 style={{color: 'white', textAlign: 'center'}}>Register</h1>
@@ -24,7 +24,42 @@ export default inject('store')(observer(({ store }) => (
         align-items: center;
         justify-content: space-between;
         flex-direction: column;
+        -webkit-animation-duration: 1s;
+        animation-duration: 1s;
+        -webkit-animation-fill-mode: both;
+        animation-fill-mode: both;
       }
+
+      @-webkit-keyframes fadeIn {
+        0% {opacity: 0;}
+        100% {opacity: 0.8;}
+      }
+
+      @keyframes fadeIn {
+        0% {opacity: 0;}
+        100% {opacity: 0.8;}
+      }
+
+      .fadeIn {
+        -webkit-animation-name: fadeIn;
+        animation-name: fadeIn;
+      }
+
+      @-webkit-keyframes fadeOut {
+        0% {opacity: 0.8;}
+        100% {opacity: 0;}
+      }
+
+      @keyframes fadeOut {
+        0% {opacity: 0.8;}
+        100% {opacity: 0;}
+      }
+
+      .fadeOut {
+        -webkit-animation-name: fadeOut;
+        animation-name: fadeOut;
+      }
+
     `}</style>
   </div>
 )))
