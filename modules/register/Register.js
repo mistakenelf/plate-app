9bdd3fbcee0d14e2d830921ce87428c0bb7a0cb5
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-01-26 22:05:12 -0600

    added registration page in modal

--- modules/register/Register.js
+++ modules/register/Register.js
@@ -1,3 +1,42 @@
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton'
+
 export default () => (
-  <h3>Register</h3>
+  <div className="register-form">
+    <h1 style={{textAlign: 'center'}}>Sign Up Now</h1>
+    <h1 style={{textAlign: 'center'}}>to Begin Serving Plates</h1>
+    <p style={{textAlign: 'center', marginTop: 20}}>Plate makes task management more simplified 
+    for your every day routine, it's free too!</p>
+
+    <div className="text-fields">
+      <TextField
+        hintText="Email"
+      /><br /><br />
+
+      <TextField
+        hintText="Password"
+      /><br />
+
+      <RaisedButton backgroundColor="#FFD54F" label="Register Now!" primary={true} style={{marginTop: 50}} />
+
+    </div>
+    
+    <style jsx>{`
+      .register-form {
+        padding: 20px;
+        border-radius: 10px;
+        width: 500px;
+        height: 500px;
+        background-color: #FAFAFA;
+        margin-left: auto;
+        margin-right: auto;
+      }
+
+      .text-fields {
+        margin-top: 50px;
+        text-align: center;
+      }
+
+    `}</style>
+  </div>
 )
