4c1d5cfb7d39bca0e40fd6706f2dddd5b91eff05
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-01-29 16:15:01 -0600

    added register modal to portal

--- components/register/Register.js
+++ components/register/Register.js
@@ -1,35 +1,38 @@
 import { inject, observer } from 'mobx-react'
-import { Button, Form, Modal } from 'semantic-ui-react'
+import { Button, Form, Portal } from 'semantic-ui-react'
 
 export default inject('store')(observer(({ store }) => (
-  <div>
-    <Modal
-      open={store.modalOpen}
-      onClose={store.closeModal}
-      className='modal-style'
-    >
-      <Modal.Header>
-        Register Now
-      </Modal.Header>
-      <Modal.Content>
-        <Form>
-          <Form.Field>
-            <label>First Name</label>
-            <input placeholder='First Name' />
-          </Form.Field>
-          <Form.Field>
-            <label>Last Name</label>
-            <input placeholder='Last Name' />
-          </Form.Field>
-          <Button type='submit' fluid>Register</Button>
-        </Form>
-      </Modal.Content>
-    </Modal>
-
+  <Portal
+    open={store.modalOpen}
+    onClose={store.closeModal}
+  >
+  <div className='register-form'>
+    <h1 style={{textAlign: 'center'}}>Sign Up Now</h1>
+    <h1 style={{textAlign: 'center'}}>to Begin Serving Plates</h1>
+    <p style={{textAlign: 'center', marginTop: 20}}>Plate makes task management more simplified
+    for your every day routine, it's free too!</p>
+    <div className='text-fields'>
+      <h3>register</h3>
+    </div>
     <style jsx>{`
-      .modal-style {
+      .register-form {
+        padding: 20px;
+        padding-top: 40px;
+        padding-left: 20px;
+        padding-right: 20px;
+        padding-bottom: 20px;
+        border-radius: 10px;
         width: 500px;
+        height: 500px;
+        background-color: #FAFAFA;
+        margin-left: auto;
+        margin-right: auto;
+      }
+      .text-fields {
+        margin-top: 50px;
+        text-align: center;
       }
     `}</style>
   </div>
+  </Portal>
 )))
