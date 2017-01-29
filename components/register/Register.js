d5d400f3153c66a03841f1a4cad48ba83479d609
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-01-29 14:55:20 -0600

    add modal width

--- components/register/Register.js
+++ components/register/Register.js
@@ -1,26 +1,35 @@
-import { Button, Form, Modal } from 'semantic-ui-react'
 import { inject, observer } from 'mobx-react'
+import { Button, Form, Modal } from 'semantic-ui-react'
 
 export default inject('store')(observer(({ store }) => (
-  <Modal
-    open={store.modalOpen}
-    onClose={store.closeModal}
-  >
-    <Modal.Header>
-      Register Now
-    </Modal.Header>
-    <Modal.Content>
-      <Form>
-        <Form.Field>
-          <label>First Name</label>
-          <input placeholder='First Name' />
-        </Form.Field>
-        <Form.Field>
-          <label>Last Name</label>
-          <input placeholder='Last Name' />
-        </Form.Field>
-        <Button type='submit' fluid>Register</Button>
-      </Form>
-    </Modal.Content>
-  </Modal>
+  <div>
+    <Modal
+      open={store.modalOpen}
+      onClose={store.closeModal}
+      className='modal-style'
+    >
+      <Modal.Header>
+        Register Now
+      </Modal.Header>
+      <Modal.Content>
+        <Form>
+          <Form.Field>
+            <label>First Name</label>
+            <input placeholder='First Name' />
+          </Form.Field>
+          <Form.Field>
+            <label>Last Name</label>
+            <input placeholder='Last Name' />
+          </Form.Field>
+          <Button type='submit' fluid>Register</Button>
+        </Form>
+      </Modal.Content>
+    </Modal>
+
+    <style jsx>{`
+      .modal-style {
+        width: 500px;
+      }
+    `}</style>
+  </div>
 )))
