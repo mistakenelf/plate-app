876cb48a63a7db566a007385cc232be18d0b84f1
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-01-29 21:57:44 -0600

    wrapped div

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import { Component } from 'react'
 import { Grid } from 'semantic-ui-react'
 import Layout from '../components/layout/Layout'
+import Banner from '../components/banner/Banner'
 
 export default () => (
-  <Layout>
-    <Grid padded>
-      <h1>Hello World</h1>
-    </Grid>
-  </Layout>
+  <div>
+    <Layout>
+      <Banner />
+    </Layout>
+  </div>
 )
