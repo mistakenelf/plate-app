533c6ed68d77942ab6c43d1f3cb40c5289509b00
knipferrc <knipferrc@gmail.com>
2017-01-29 18:24:49 -0600

    minor fixes

--- components/register/Register.js
+++ components/register/Register.js
@@ -1,40 +1,41 @@
 import { inject, observer } from 'mobx-react'
-import { Button, Form, Dimmer } from 'semantic-ui-react'
+
+import { Dimmer } from 'semantic-ui-react'
 
 export default inject('store')(observer(({ store }) => (
   <div>
-      <Dimmer active={store.modalOpen} 
-              onClickOutside={store.closeModal} 
+    <Dimmer active={store.modalOpen}
+      onClickOutside={store.closeModal}
+      style={{position: 'fixed'}}
       >
-        <div className='register-form'>
-          <h1 style={{textAlign: 'center'}}>Sign Up Now</h1>
-          <h1 style={{textAlign: 'center'}}>to Begin Serving Plates</h1>
-          <p style={{textAlign: 'center', marginTop: 20}}>Plate makes task management more simplified
+      <div className='register-form'>
+        <h1>Sign Up Now</h1>
+        <h1>to Begin Serving Plates</h1>
+        <p style={{marginTop: 20 }}>Plate makes task management more simplified
           for your every day routine, it's free too!</p>
-          <div className='text-fields'>
-            <h3>register</h3>
-          </div>
-          <style jsx>{`
-            .register-form {
-              color: black;
-              padding: 20px;
-              padding-top: 40px;
-              padding-left: 20px;
-              padding-right: 20px;
-              padding-bottom: 20px;
-              border-radius: 10px;
-              width: 500px;
-              height: 500px;
-              background-color: #FAFAFA;
-              margin-left: auto;
-              margin-right: auto;
-            }
-            .text-fields {
-              margin-top: 50px;
-              text-align: center;
-            }
-          `}</style>
+        <div className='text-fields'>
+          <h3>register</h3>
         </div>
-      </Dimmer>
+      </div>
+    </Dimmer>
+    <style jsx>{`
+      .register-form {
+        color: black;
+        padding: 20px;
+        padding-top: 40px;
+        padding-left: 20px;
+        padding-right: 20px;
+        padding-bottom: 20px;
+        border-radius: 10px;
+        background-color: #FAFAFA;
+        margin-left: auto;
+        margin-right: auto;
+        text-align: center;
+      }
+      .text-fields {
+        margin-top: 50px;
+        text-align: center;
+      }
+    `}</style>
   </div>
 )))
