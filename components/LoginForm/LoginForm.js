348fcf940a3fc9b8ce373ecea4940969a3112ebf
knipferrc <knipferrc@gmail.com>
2017-02-01 08:45:45 -0600

    login fixes

--- components/LoginForm/LoginForm.js
+++ components/LoginForm/LoginForm.js
@@ -2,23 +2,25 @@ import { Button, Divider, Form, Grid, Icon } from 'semantic-ui-react'
 
 export default () => (
   <div className='login-form'>
-    <Grid divided='vertically'>
+    <Grid centered>
       <Grid.Row>
-        <Divider vertical>or</Divider>
-        <Grid.Column computer={8} mobile={16}>
+        <Grid.Column computer={8} tablet={16} mobile={16}>
           <h3>Login Into Your Account</h3>
           <Form>
-            <Form.Field style={{width: '300px'}}>
+            <Form.Field>
               <input type='text' name='username' id='username' placeholder='username' required />
             </Form.Field>
-            <Form.Field style={{width: '300px', marginTop: '20px', marginBottom: '20px'}}>
+            <Form.Field>
               <input type='text' name='email' id='email' placeholder='email' required />
             </Form.Field>
-            <Button type='submit' primary style={{width: '300px'}}>Register Now!</Button>
+            <Button type='submit' primary>Register Now!</Button>
             <p><a href='#' className='link'>Forgot Password?</a></p>
           </Form>
         </Grid.Column>
-        <Grid.Column computer={8} mobile={16}>
+        <Grid.Column computer={1} only='computer'>
+          <Divider vertical>or</Divider>
+        </Grid.Column>
+        <Grid.Column computer={7} mobile={16}>
           <div className='buttons'>
             <Button color='facebook' style={{marginBottom: '15px', width: '300px'}}>
               <Icon name='facebook' /> Sign in with Facebook
@@ -37,25 +39,20 @@ export default () => (
     </Grid>
     <style jsx>{`
       .login-form {
-        padding-top: 30px;
-        padding-left: 40px;
-        padding-right: 40px;
-        margin-left: auto;
-        margin-right: auto;
-        margin-top: 140px;
-        width: 60%;
-        height: 320px;
         background-color: #F7F7F7;
         border-radius: 10px;
+        padding: 50px;
+        margin-top: 50px;
+        width: 100%;
+        display: flex;
+        justify-content: center;
       }
       .link {
         margin-top: 10px;
         padding-top: 10px;
-        float: right;
       }
       .buttons {
         margin-top: 50px;
-        float: right;
       }
     `}</style>
   </div>
