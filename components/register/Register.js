6390a99d7a360822d5771a4af14ed999f1724956
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-02-01 00:02:50 -0600

    adjusted register modal

--- components/register/Register.js
+++ components/register/Register.js
@@ -1,4 +1,4 @@
-import { Button, Dimmer, Divider, Form } from 'semantic-ui-react'
+import { Button, Dimmer, Divider, Form, Icon } from 'semantic-ui-react'
 import { inject, observer } from 'mobx-react'
 
 import { Component } from 'react'
@@ -11,7 +11,7 @@ export default class extends Component {
 
     this.props.store.startLoading()
 
-    const username = document.getElementById('username').value
+    const username = document.getElementById('email').value
     const password = document.getElementById('password').value
 
     const promise = auth.createUserWithEmailAndPassword(username, password)
@@ -29,17 +29,25 @@ export default class extends Component {
           page
           >
           <div className='register-form'>
-            <h1>Sign Up Now <br />
-            to Begin Serving Plates</h1>
-            <p style={{marginTop: 20}}>Create endless tasks for free!</p>
+            <Icon name='add user' size='huge' />
+            <h3 style={{marginTop: '10px', marginBottom: '0px'}}>Register</h3>
+            <h5 style={{marginTop: '5px', marginBottom: '25px'}}>Sign up for free.</h5>
             <Form method='post' onSubmit={this.register} loading={this.props.store.loading}>
               <Form.Field>
                 <input type='text' name='username' id='username' placeholder='username' required />
               </Form.Field>
 
               <Form.Field>
+                <input type='text' name='email' id='email' placeholder='email' required />
+              </Form.Field>
+
+              <Form.Field>
                 <input type='password' name='password' id='password' placeholder='password' required />
               </Form.Field>
+              <Form.Field>
+                <input type='password' name='confirmPassword' id='confirmPassword' placeholder='confirm password' required />
+              </Form.Field>
+
               <Divider />
               <Button type='submit' primary>Register Now!</Button>
             </Form>
@@ -49,16 +57,16 @@ export default class extends Component {
           .register-form {
             color: black;
             padding: 20px;
-            padding-top: 40px;
+            padding-top: 20px;
             padding-left: 20px;
             padding-right: 20px;
             padding-bottom: 20px;
-            border-radius: 10px;
+            border-radius: 5px;
             background-color: #FAFAFA;
             margin-left: auto;
             margin-right: auto;
             text-align: center;
-            width: 500px;
+            width: 400px;
           }
           @media only screen and (max-width: 660px) {
             .register-form {
