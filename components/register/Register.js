05788a2d3590b5cfad2a3c2f1f45ac9687739bb3
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-01-31 20:26:30 -0600

    add users

--- components/register/Register.js
+++ components/register/Register.js
@@ -1,47 +1,72 @@
-import { Button, Dimmer, Divider, Input } from 'semantic-ui-react'
+import { Button, Dimmer, Divider, Form } from 'semantic-ui-react'
 import { inject, observer } from 'mobx-react'
 
-export default inject('store')(observer(({ store }) => (
-  <div>
-    <Dimmer active={store.modalOpen}
-      onClickOutside={store.closeModal}
-      page
-      >
-      <div className='register-form'>
-        <h1>Sign Up Now <br />
-        to Begin Serving Plates</h1>
-        <p style={{marginTop: 20}}>Create endless tasks for free!</p>
-        <Input placeholder='username' size='large' style={{marginBottom: '10px'}} />
-        <br />
-        <Input type='password' placeholder='password' size='large' style={{marginBottom: '20px'}} />
-        <br />
-        <Divider />
-        <Button primary>Register Now!</Button>
+import { Component } from 'react'
+import { auth } from '../../lib/db'
+
+@inject('store') @observer
+export default class extends Component {
+  register = (e) => {
+    e.preventDefault()
+
+    const username = document.getElementById('username').value
+    const password = document.getElementById('password').value
+
+    const promise = auth.createUserWithEmailAndPassword(username, password)
+
+    promise
+      .then(this.props.store.closeModal)
+  }
+
+  render () {
+    return (
+      <div>
+        <Dimmer active={this.props.store.modalOpen}
+          onClickOutside={this.props.store.closeModal}
+          page
+          >
+          <div className='register-form'>
+            <h1>Sign Up Now <br />
+            to Begin Serving Plates</h1>
+            <p style={{marginTop: 20}}>Create endless tasks for free!</p>
+            <Form method='post' onSubmit={this.register}>
+              <Form.Field>
+                <input type='text' name='username' id='username' placeholder='username' required />
+              </Form.Field>
+
+              <Form.Field>
+                <input type='password' name='password' id='password' placeholder='password' required />
+              </Form.Field>
+              <Divider />
+              <Button type='submit' primary>Register Now!</Button>
+            </Form>
+          </div>
+        </Dimmer>
+        <style jsx>{`
+          .register-form {
+            color: black;
+            padding: 20px;
+            padding-top: 40px;
+            padding-left: 20px;
+            padding-right: 20px;
+            padding-bottom: 20px;
+            border-radius: 10px;
+            background-color: #FAFAFA;
+            margin-left: auto;
+            margin-right: auto;
+            text-align: center;
+            width: 500px;
+          }
+          @media only screen and (max-width: 660px) {
+            .register-form {
+              width: 95%;
+              padding-right: 10px;
+              padding-left: 10px;
+              padding-top: 20px;
+              padding-bottom: 20px;
+            }
+        `}</style>
       </div>
-    </Dimmer>
-    <style jsx>{`
-      .register-form {
-        color: black;
-        padding: 20px;
-        padding-top: 40px;
-        padding-left: 20px;
-        padding-right: 20px;
-        padding-bottom: 20px;
-        border-radius: 10px;
-        background-color: #FAFAFA;
-        margin-left: auto;
-        margin-right: auto;
-        text-align: center;
-        width: 500px;
-      }
-      @media only screen and (max-width: 660px) {
-        .register-form {
-          width: 95%;
-          padding-right: 10px;
-          padding-left: 10px;
-          padding-top: 20px;
-          padding-bottom: 20px;
-        }
-    `}</style>
-  </div>
-)))
+    )
+  }
+}
