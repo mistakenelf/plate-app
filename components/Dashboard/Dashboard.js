878106f036592d974797801bcd417175e5d94935
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-02-04 20:33:03 -0600

    alert shows current display name

--- components/Dashboard/Dashboard.js
+++ components/Dashboard/Dashboard.js
@@ -1,20 +1,31 @@
 import { inject, observer } from 'mobx-react'
 
+import { Component } from 'react'
 import { Message } from 'semantic-ui-react'
+import ReactTextEditor from 'react-text-editor'
+import { auth } from '../../lib/db'
 
-export default inject('store')(observer(({ store }) => {
-  return (
-    <div>
-      <Message
-        floating='true'
-        success='true'
-        visible={store.registerMessage}
-        hidden={store.registerMessageHidden}
-        onDismiss={store.hideRegisterMessage}
-        header='Welcome to plate!'
-        content='This is your dashboard where you can add new plates.'
-        positioning='top center'
-      />
-    </div>
-  )
-}))
+@inject('store') @observer
+export default class extends Component {
+  componentDidMount() {
+    this.props.store.displayUserName = auth.currentUser.displayName
+  }
+
+  render() {
+    return (
+      <div>
+        <Message
+          floating='true'
+          success='true'
+          visible={this.props.store.registerMessage}
+          hidden={this.props.store.registerMessageHidden}
+          onDismiss={this.props.store.hideRegisterMessage}
+          header={'Hello, ' + this.props.store.displayUserName}
+          content='This is your dashboard where you can add new plates.'
+          positioning='top center'
+        />
+        <ReactTextEditor />
+      </div>
+    )
+  }
+}
