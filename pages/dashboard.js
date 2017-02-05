b070ee0415d30e468560426dc20c1fade8c80942
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-02-04 19:54:07 -0600

    import dash component

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,10 +1,11 @@
+import Dashboard from '../components/Dashboard/Dashboard'
 import Layout from '../components/Layout/Layout'
 import ReactTextEditor from 'react-text-editor'
 
 export default () => (
   <Layout>
     <div style={{margin: 50}}>
-      <ReactTextEditor />
+      <Dashboard />
     </div>
   </Layout>
 )
