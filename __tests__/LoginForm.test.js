e447e9a4de5c7fbd3572d916f41573eed7b1ac9a
knipferrc <knipferrc@gmail.com>
2017-02-06 14:46:11 -0600

    fix test

--- __tests__/LoginForm.test.js
+++ __tests__/LoginForm.test.js
@@ -1,17 +1,26 @@
 /* global it, expect, describe */
 
+import Layout from '../components/Layout/Layout'
 import LoginForm from '../components/LoginForm/LoginForm'
 import { render } from 'react-dom'
 import renderer from 'react-test-renderer'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  render(<LoginForm />, div)
+  render(
+    <Layout>
+      <LoginForm />
+    </Layout>,
+    div)
 })
 
-describe('Login snapshot', () => {
+describe('LoginForm snapshot', () => {
   it('renders without crashing!"', () => {
-    const component = renderer.create(<LoginForm />)
+    const component = renderer.create(
+      <Layout>
+        <LoginForm />
+      </Layout>
+    )
     const tree = component.toJSON()
     expect(tree).toMatchSnapshot()
   })
