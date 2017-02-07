d0db3be2362c1f1726fd738e5f7899a9dc450a3b
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-02-07 00:17:01 -0600

    route to unauthorized

--- components/AuthRequired/AuthRequired.js
+++ components/AuthRequired/AuthRequired.js
@@ -2,11 +2,12 @@ import { inject, observer } from 'mobx-react'
 
 import { Component } from 'react'
 import Router from 'next/router'
+import Unauthorized from '../Unauthorized/Unauthorized'
 
 export default inject('store')(observer(class extends Component {
   componentDidMount () {
     if (!this.props.store.loggedIn) {
-      Router.push('/login')
+      Router.push('/unauthorized')
     }
   }
 
