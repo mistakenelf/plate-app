27164616461265d1caaa93c6360ea81323256387
knipferrc <knipferrc@gmail.com>
2017-02-07 14:35:07 -0600

    fix alignment

--- components/ForgotPassword/ForgotPassword.js
+++ components/ForgotPassword/ForgotPassword.js
@@ -1,38 +1,28 @@
 import { Button, Form, Grid } from 'semantic-ui-react'
 
 export default () => (
-  <div className='forgot-password-form'>
-    <Grid padded>
-      <Grid.Row>
-        <Grid.Column>
+  <Grid textAlign='left'>
+    <Grid.Row centered>
+      <Grid.Column width={16} computer={8} widescreen={8} tablet={10} mobile={14}>
+        <div className='forgot-password-form'>
           <h2>Retrieve Password</h2>
-        </Grid.Column>
-      </Grid.Row>
-      <Grid.Row>
-        <Grid.Column>
           <p>
             Enter the email associated with your account and a link will be sent
-            to your email with instructions on resetting your password.
-          </p>
-        </Grid.Column>
-      </Grid.Row>
-      <Grid.Row>
-        <Grid.Column width={16} computer={5} widescreen={5} tablet={16} mobile={16}>
+          to your email with instructions on resetting your password.
+        </p>
           <Form>
             <Form.Field>
               <input type='text' name='email' id='email' placeholder='email' required />
             </Form.Field>
             <Button type='submit' primary>Reset</Button>
           </Form>
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
+        </div>
+      </Grid.Column>
+    </Grid.Row>
     <style jsx>{`
-      .forgot-password-form {
-        margin-top: 80px;
-        margin-left: auto;
-        margin-right: auto;
-      }
-    `}</style>
-  </div>
+    .forgot-password-form {
+      margin-top: 80px;
+    }
+  `}</style>
+  </Grid>
 )
