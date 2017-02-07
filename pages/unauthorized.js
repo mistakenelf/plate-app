7298818ec32b4ae7684553a3f7c96e59b288a22a
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-02-07 00:17:19 -0600

    unauthorized page

---
+++ pages/unauthorized.js
@@ -0,0 +1,8 @@
+import Layout from '../components/Layout/Layout'
+import Unauthorized from '../components/Unauthorized/Unauthorized'
+
+export default () => (
+  <Layout>
+      <Unauthorized />
+  </Layout>
+)
