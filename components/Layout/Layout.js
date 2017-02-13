f2cd2fce712827af6796d505322d2e9231389a36
knipferrc <knipferrc@gmail.com>
2017-02-13 07:58:57 -0600

    remove CDN

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -20,7 +20,7 @@ export default ({ children, open, openMenu, closeMenu }) => (
       <meta name='theme-color' content='#00B5AD' />
       <meta name='apple-mobile-web-app-status-bar-style' content='black-translucent' />
       <link rel='shortcut icon' href='/static/plateLogo.png' />
-      <link rel='stylesheet' href='//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css' />
+      <link rel='stylesheet' href='/static/semantic.min.css' />
       <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500' rel='stylesheet' />
     </Head>
     <Navigation
