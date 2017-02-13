3eea346c83c60bf4204f9dbffac41cf22ec616f9
knipferrc <knipferrc@gmail.com>
2017-02-13 13:59:51 -0600

    remove data fetch

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const next = require('next')
 
-const data = require('./data.json')
-
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
@@ -10,10 +8,6 @@ const handle = app.getRequestHandler()
 app.prepare()
   .then(() => {
     const server = express()
-
-    server.get('/api/requests', (request, response, next) => {
-      response.send(data)
-    })
 
     server.get('*', (req, res) => {
       return handle(req, res)
