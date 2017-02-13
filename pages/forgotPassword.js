dd3890fbcece1ea4506ceaa46b16b69bbcab9aac
knipferrc <knipferrc@gmail.com>
2017-02-13 10:05:33 -0600

    create a data HOC

--- pages/forgotPassword.js
+++ pages/forgotPassword.js
@@ -1,30 +1,9 @@
-import rootReducer, { client } from '../redux/rootReducer'
-
-import { ApolloProvider } from 'react-apollo'
-import { Component } from 'react'
 import ForgotPassword from '../components/ForgotPassword/ForgotPassword'
 import LayoutContainer from '../containers/LayoutContainer'
-import { initStore } from '../redux/createStore'
+import withData from '../lib/withData'
 
-export default class extends Component {
-  static getInitialProps ({ req }) {
-    const isServer = !!req
-    const store = initStore(rootReducer, {}, isServer)
-    return { initialState: store.getState(), isServer }
-  }
-
-  constructor (props) {
-    super(props)
-    this.store = initStore(rootReducer, props.initialState, props.isServer)
-  }
-
-  render () {
-    return (
-      <ApolloProvider store={this.store} client={client}>
-        <LayoutContainer>
-          <ForgotPassword />
-        </LayoutContainer>
-      </ApolloProvider>
-    )
-  }
-}
+export default withData(() => (
+  <LayoutContainer>
+    <ForgotPassword />
+  </LayoutContainer>
+))
--- pages/login.js
+++ pages/login.js
@@ -1,30 +1,9 @@
-import rootReducer, { client } from '../redux/rootReducer'
-
-import { ApolloProvider } from 'react-apollo'
-import { Component } from 'react'
 import LayoutContainer from '../containers/LayoutContainer'
 import LoginForm from '../components/LoginForm/LoginForm'
-import { initStore } from '../redux/createStore'
+import withData from '../lib/withData'
 
-export default class extends Component {
-  static getInitialProps ({ req }) {
-    const isServer = !!req
-    const store = initStore(rootReducer, {}, isServer)
-    return { initialState: store.getState(), isServer }
-  }
-
-  constructor (props) {
-    super(props)
-    this.store = initStore(rootReducer, props.initialState, props.isServer)
-  }
-
-  render () {
-    return (
-      <ApolloProvider store={this.store} client={client}>
-        <LayoutContainer>
-          <LoginForm />
-        </LayoutContainer>
-      </ApolloProvider>
-    )
-  }
-}
+export default withData(() => (
+  <LayoutContainer>
+    <LoginForm />
+  </LayoutContainer>
+))
