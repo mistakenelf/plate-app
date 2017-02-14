fa26adfe137c4536f61b55bd58312bd38033d306
knipferrc <knipferrc@gmail.com>
2017-02-14 13:27:14 -0600

    basic graphql setup

--- server/main.js
+++ server/main.js
@@ -1,13 +1,33 @@
 const express = require('express')
 const next = require('next')
+const graphqlHTTP = require('express-graphql')
+const { buildSchema } = require('graphql')
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`)
+
+const root = {
+  hello: () => {
+    return 'hello world'
+  }
+}
+
 app.prepare()
   .then(() => {
     const server = express()
+
+    server.use('/graphql', graphqlHTTP({
+      schema: schema,
+      rootValue: root,
+      graphiql: dev
+    }))
 
     server.get('*', (req, res) => {
       return handle(req, res)
