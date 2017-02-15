295819890946c8ed352392381cc5abc99f9ec340
knipferrc <knipferrc@gmail.com>
2017-02-15 17:26:17 -0600

    move links to right nav

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,31 +1,47 @@
 import AppBar from 'material-ui/AppBar'
 import Drawer from 'material-ui/Drawer'
+import IconButton from 'material-ui/IconButton'
+import IconMenu from 'material-ui/IconMenu'
 import Link from 'next/prefetch'
 import MenuItem from 'material-ui/MenuItem'
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert'
 import { indigo500 } from 'material-ui/styles/colors'
 
-export default ({ menuOpen, openMenu, closeMenu }) => (
-  <div>
-    <AppBar
-      title='Plate'
-      onLeftIconButtonTouchTap={() => openMenu()}
-      style={{ backgroundColor: indigo500 }}
-    />
-    <Drawer
-      docked={false}
-      width={200}
-      open={menuOpen}
-      onRequestChange={() => closeMenu()}
+export default ({ menuOpen, openMenu, closeMenu }) => {
+  const elementRight = (
+    <IconMenu
+      iconButtonElement={
+        <IconButton><MoreVertIcon /></IconButton>
+      }
+      targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+      anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
     >
-      <Link href='/'><a><MenuItem onTouchTap={() => closeMenu()}>Home</MenuItem></a></Link>
       <Link href='/login'><a><MenuItem onTouchTap={() => closeMenu()}>Login</MenuItem></a></Link>
       <Link href='/register'><a><MenuItem onTouchTap={() => closeMenu()}>Register</MenuItem></a></Link>
-    </Drawer>
-    <style jsx global>{`
-      a {
-        text-decoration: none;
-        text-align: center;
-      }
-    `}</style>
-  </div>
-)
+    </IconMenu>
+  )
+
+  return (
+    <div>
+      <AppBar
+        title='Plate'
+        onLeftIconButtonTouchTap={() => openMenu()}
+        iconElementRight={elementRight}
+        style={{ backgroundColor: indigo500 }}
+      />
+      <Drawer
+        docked={false}
+        width={200}
+        open={menuOpen}
+        onRequestChange={() => closeMenu()}
+      >
+        <Link href='/'><a><MenuItem onTouchTap={() => closeMenu()}>Home</MenuItem></a></Link>
+      </Drawer>
+      <style jsx global>{`
+        a {
+          text-decoration: none;
+        }
+      `}</style>
+    </div>
+  )
+}
