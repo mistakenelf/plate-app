2f01aa6476ffdfa07d24eefaf9d603b3c5351fa7
knipferrc <knipferrc@gmail.com>
2017-02-15 14:42:18 -0600

    use material ui

--- components/LoginForm/LoginForm.js
+++ components/LoginForm/LoginForm.js
@@ -1,7 +1,9 @@
-import { Button, Form, Grid } from 'semantic-ui-react'
+import { Col, Container, Row } from 'react-grid-system'
 
 import Link from 'next/prefetch'
 import Message from '../Message/Message'
+import RaisedButton from 'material-ui/RaisedButton'
+import TextField from 'material-ui/TextField'
 
 const login = (e, showMessage) => {
   e.preventDefault()
@@ -9,24 +11,23 @@ const login = (e, showMessage) => {
 }
 
 export default ({ messageOpen, showMessage, hideMessage }) => (
-  <Grid padded centered>
-    <Grid.Row>
-      <Grid.Column mobile={16} tablet={10} computer={6} largeScreen={6}>
-        {messageOpen &&
-          <Grid.Row>
-            <Grid.Column mobile={16} tablet={10} computer={6} largeScreen={6}>
-              <Message message='Error processing your request' />
-            </Grid.Column>
-          </Grid.Row>
-        }
-        <h2>Login</h2>
-        <Form onSubmit={(e) => login(e, showMessage)}>
-          <Form.Input type='text' name='email' placeholder='email' label='Email' />
-          <Form.Input type='password' name='password' placeholder='password' label='Password' />
-          <Button primary fluid>Login</Button>
-        </Form>
-      </Grid.Column>
-    </Grid.Row>
-    <Link href='/forgotPassword'><a>Forgot Password?</a></Link>
-  </Grid>
+  <Container fluid>
+    <Row>
+      <Col sm={12} md={8} lg={6} offset={{ lg: 3}}>
+        <Message message='Failed to process request' />
+        <h3>Login</h3>
+        <form onSubmit={(e) => login(e, showMessage)}>
+          <TextField hintText='email' type='text' fullWidth />
+          <TextField hintText='password' type='password' fullWidth />
+          <RaisedButton
+            secondary
+            fullWidth
+            label='Login'
+            type='submit'
+          />
+        </form>
+        <Link href='/forgotPassword'><a>Forgot Password?</a></Link>
+      </Col>
+    </Row>
+  </Container>
 )
