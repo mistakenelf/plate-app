723ec703564af37fb4f815c0b61b25bdf8aebef5
knipferrc <knipferrc@gmail.com>
2017-02-15 13:51:19 -0600

    change port

--- server/main.js
+++ server/main.js
@@ -46,8 +46,8 @@ app.prepare()
       return handle(req, res)
     })
 
-    server.listen(3000, (err) => {
+    server.listen(4000, (err) => {
       if (err) throw err
-      console.log('> Ready on http://localhost:3000')
+      console.log('> Ready on http://localhost:4000')
     })
   })
