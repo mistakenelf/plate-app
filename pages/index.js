18542f3f9c36ad8951cc34e1e76ac47f24c9fba5
knipferrc <knipferrc@gmail.com>
2017-02-16 13:31:28 -0600

    remove footer

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import AdvertisementCards from '../components/AdvertisementCards/AdvertisementCards'
 import Banner from '../components/Banner/Banner'
-import Footer from '../components/Footer/Footer'
 import LayoutContainer from '../containers/LayoutContainer'
 import pageDecorator from '../lib/pageDecorator'
 
@@ -8,6 +7,5 @@ export default pageDecorator(() => (
   <LayoutContainer>
     <Banner />
     <AdvertisementCards />
-    <Footer />
   </LayoutContainer>
 ))
