d9b0ffc6312535d1c17538ebdb92b8a5596f53f4
knipferrc <knipferrc@gmail.com>
2017-02-17 10:00:12 -0600

    comments

--- store/middleware.js
+++ store/middleware.js
@@ -1,5 +1,6 @@
 import { applyMiddleware, compose } from 'redux'
 
+// Create our middleware and setup devtools
 export default function createMiddleware (clientMiddleware) {
   const middleware = applyMiddleware(clientMiddleware)
   if (process.browser && window.devToolsExtension) {
--- store/reducer.js
+++ store/reducer.js
@@ -2,6 +2,8 @@ import { combineReducers } from 'redux'
 import drawer from './modules/drawer'
 import loginForm from './modules/loginForm'
 
+// Root reducer for our redux store and
+// apollo's store
 export default function getReducer (client) {
   return combineReducers({
     drawer,
