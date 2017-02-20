52ceaf0fb16c7f8eefe494f1d76f760cb351c1ad
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-02-19 21:09:50 -0600

    login page layout

--- components/LoginForm/LoginForm.js
+++ components/LoginForm/LoginForm.js
@@ -13,28 +13,23 @@ export default ({ messageOpen, showMessage, hideMessage }) => (
   <div className='container-fluid'>
     <div className='row full-height middle-xs middle-sm middle-md middle-lg middle-xl center-xs center-sm center-md center-lg center-lg'>
       <div className='col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-4'>
-        <Card>
-          <CardText>
-            <h3>Login</h3>
-            <form onSubmit={login}>
-              <div className='text-field-email'>
-                <TextField hintText='email' id='email' type='text' autoComplete='off' fullWidth />
-              </div>
-              <div className='text-field-password'>
-                <TextField hintText='password' id='password' type='password' fullWidth />
-              </div>
-              <RaisedButton
-                secondary
-                fullWidth
-                label='Login'
-                type='submit'
-              />
-            </form>
-            <div className='forgot-password'>
-              <Link prefetch href='/forgotPassword'><a>Forgot Password?</a></Link>
+        <h3 style={{color: 'white', marginBottom: '50px'}}>Login</h3>
+        <form onSubmit={login}>
+          <div className='text-field-email'>
+            <TextField hintText='email' id='email' type='text' autoComplete='off' fullWidth />
+          </div>
+          <div className='text-field-password'>
+            <TextField hintText='password' id='password' type='password' fullWidth />
+          </div>
+          <button type='button' className='submit-button'>
+            <div className='button-text'>
+              <h3>Login</h3>
             </div>
-          </CardText>
-        </Card>
+          </button>
+        </form>
+        <div className='forgot-password'>
+          <Link prefetch href='/forgotPassword'><a>Forgot Password?</a></Link>
+        </div>
       </div>
     </div>
     <style jsx>{`
@@ -47,8 +42,25 @@ export default ({ messageOpen, showMessage, hideMessage }) => (
       .forgot-password {
         margin-top: 15px;
       }
+      .forgot-password a {
+        color: white;
+        text-decoration: none;
+      }
       .full-height {
         height: 90vh;
+      }
+      .submit-button {
+        background: none;
+        border: 1px solid white;
+        border-radius: 3px;
+        left: 150px;
+        top: 80px;
+        width: 30%;
+        height: 30px;
+        margin-top: 30px;
+      }
+      .button-text {
+        color: white;
       }
     `}</style>
   </div>
