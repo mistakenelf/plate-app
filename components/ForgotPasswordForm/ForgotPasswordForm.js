b681f97642ac2c4f014f4d50e44e0855fea5e0a4
knipferrc <knipferrc@gmail.com>
2017-02-20 11:09:39 -0600

    fix forms

--- components/ForgotPasswordForm/ForgotPasswordForm.js
+++ components/ForgotPasswordForm/ForgotPasswordForm.js
@@ -1,6 +1,4 @@
-import { Card, CardText } from 'material-ui/Card'
-
-import RaisedButton from 'material-ui/RaisedButton'
+import FlatButton from 'material-ui/FlatButton'
 import TextField from 'material-ui/TextField'
 
 const forgotPassword = (e) => {
@@ -12,25 +10,35 @@ export default () => (
   <div className='container-fluid'>
     <div className='row full-height middle-xs middle-sm middle-md middle-lg middle-xl center-xs center-sm center-md center-lg center-lg'>
       <div className='col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-4'>
-        <Card>
-          <CardText>
-            <h3>Forgot Password</h3>
-            <form onSubmit={forgotPassword}>
-              <TextField hintText='email' id='email' type='text' fullWidth />
-              <RaisedButton
-                secondary
-                fullWidth
-                label='Forgot Password'
-                type='submit'
-              />
-            </form>
-          </CardText>
-        </Card>
+        <h3 style={{ color: 'white', marginBottom: 50 }}>Forgot Password</h3>
+        <form onSubmit={forgotPassword}>
+          <div className='text-field-email'>
+            <TextField
+              hintText='email'
+              id='email'
+              hintStyle={{ color: 'white' }}
+              type='text'
+              inputStyle={{ color: 'white' }}
+              autoComplete={'off'}
+              fullWidth
+              required
+            />
+          </div>
+          <FlatButton
+            type='submit'
+            label='Forgot Password'
+            labelStyle={{ color: 'white' }}
+            style={{ border: '1px solid white' }}
+          />
+        </form>
       </div>
     </div>
     <style jsx>{`
       .full-height {
         height: 90vh;
+      }
+      .text-field-email {
+        margin-bottom: 15px;
       }
     `}</style>
   </div>
--- components/RegisterForm/RegisterForm.js
+++ components/RegisterForm/RegisterForm.js
@@ -1,6 +1,4 @@
-import { Card, CardText } from 'material-ui/Card'
-
-import RaisedButton from 'material-ui/RaisedButton'
+import FlatButton from 'material-ui/FlatButton'
 import TextField from 'material-ui/TextField'
 
 const register = (e) => {
@@ -12,26 +10,50 @@ export default () => (
   <div className='container-fluid'>
     <div className='row full-height middle-xs middle-sm middle-md middle-lg middle-xl center-xs center-sm center-md center-lg center-lg'>
       <div className='col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-4'>
-        <Card>
-          <CardText>
-            <h3>Register</h3>
-            <form onSubmit={register}>
-              <TextField hintText='email' id='email' type='text' fullWidth />
-              <TextField hintText='password' id='password' type='password' fullWidth />
-              <RaisedButton
-                secondary
-                fullWidth
-                label='Register'
-                type='submit'
-              />
-            </form>
-          </CardText>
-        </Card>
+        <h3 style={{ color: 'white', marginBottom: 50 }}>Register</h3>
+        <form onSubmit={register}>
+          <div className='text-field-email'>
+            <TextField
+              hintText='email'
+              id='email'
+              hintStyle={{ color: 'white' }}
+              type='text'
+              inputStyle={{ color: 'white' }}
+              autoComplete={'off'}
+              fullWidth
+              required
+            />
+          </div>
+          <div className='text-field-password'>
+            <TextField
+              hintText='password'
+              id='password'
+              hintStyle={{ color: 'white' }}
+              type='password'
+              inputStyle={{ color: 'white' }}
+              autoComplete={'off'}
+              fullWidth
+              required
+            />
+          </div>
+          <FlatButton
+            type='submit'
+            label='Register'
+            labelStyle={{ color: 'white' }}
+            style={{ border: '1px solid white' }}
+          />
+        </form>
       </div>
     </div>
     <style jsx>{`
       .full-height {
         height: 90vh;
+      }
+      .text-field-email {
+        margin-bottom: 15px;
+      }
+      .text-field-password {
+        margin-bottom: 15px;
       }
     `}</style>
   </div>
