db5bf3349c4c7165f761386198ffebb08769991d
knipferrc <knipferrc@gmail.com>
2017-02-24 16:18:59 -0600

    setup form

--- components/AddPlateDialog/AddPlateDialog.js
+++ components/AddPlateDialog/AddPlateDialog.js
@@ -1,12 +1,35 @@
 import Dialog from 'material-ui/Dialog'
+import FlatButton from 'material-ui/FlatButton'
+import TextField from 'material-ui/TextField'
 
-export default ({ open, openDialog, closeDialog }) => (
-  <Dialog
-    title='Add A New Plate'
-    modal={false}
-    open={open}
-    onRequestClose={closeDialog}
-  >
-    This will be how you add a new plate
-  </Dialog>
-)
+export default ({ open, openDialog, closeDialog }) => {
+  const actions = [
+    <FlatButton
+      label='Cancel'
+      primary
+      onTouchTap={closeDialog}
+    />,
+    <FlatButton
+      label='Submit'
+      primary
+      keyboardFocused
+    />
+  ]
+
+  return (
+    <Dialog
+      title='Add A New Plate'
+      modal={false}
+      open={open}
+      actions={actions}
+      onRequestClose={closeDialog}
+    >
+      <TextField
+        hintText='Name' fullWidth
+      /><br />
+      <TextField
+        hintText='Description' fullWidth rows={4} rowsMax={4} multiLine
+      /><br />
+    </Dialog>
+  )
+}
