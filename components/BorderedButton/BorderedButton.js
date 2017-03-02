eca6bc6dcd968ae506194442cf7a582d76138ed9
knipferrc <knipferrc@gmail.com>
2017-03-02 12:58:40 -0600

    BorderedButton component

---
+++ components/BorderedButton/BorderedButton.js
@@ -0,0 +1,18 @@
+import FlatButton from 'material-ui/FlatButton'
+
+export default ({ type, label }) => (
+  <div className='bordered-button'>
+    <FlatButton
+      type={type}
+      label={label}
+      labelStyle={{ color: 'white', width: '100%' }}
+    />
+    <style jsx>{`
+      .bordered-button {
+        border: 1px solid white;
+        border-radius: 4px;
+        display: inline-block;
+      }
+    `}</style>
+  </div>
+)
