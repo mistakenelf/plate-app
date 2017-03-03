1230d515c374d611cc407988c42e0c30b5a1db20
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-03-03 12:24:42 -0600

    use toolbar

--- components/DashboardMenu/DashboardMenu.js
+++ components/DashboardMenu/DashboardMenu.js
@@ -1,10 +1,10 @@
-import {BottomNavigation, BottomNavigationItem} from 'material-ui/BottomNavigation'
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
 
-import Add from 'material-ui/svg-icons/content/add'
 import AddPlateDialog from '../AddPlateDialog/AddPlateDialog'
 import Block from 'material-ui/svg-icons/content/block'
+import {BottomNavigationItem} from 'material-ui/BottomNavigation'
 import DoneAll from 'material-ui/svg-icons/action/done-all'
-import Paper from 'material-ui/Paper'
+import RaisedButton from 'material-ui/RaisedButton'
 
 type Props = {
   open?: boolean,
@@ -13,26 +13,28 @@ type Props = {
 }
 
 export default ({open, openDialog, closeDialog}: Props) => (
-  <Paper zDepth={1}>
-    <BottomNavigation>
-      <BottomNavigationItem
-        label='Remove All'
-        icon={<Block />}
-      />
-      <BottomNavigationItem
-        label='Mark All Complete'
-        icon={<DoneAll />}
-      />
-      <BottomNavigationItem
-        label='Add Plate'
-        icon={<Add />}
-        onTouchTap={openDialog}
-      />
-    </BottomNavigation>
+  <div className='toolbar'>
+    <Toolbar>
+      <ToolbarGroup>
+        <ToolbarTitle text='Manage Plates' />
+      </ToolbarGroup>
+      <ToolbarGroup>
+        <BottomNavigationItem
+          label='Remove All'
+          icon={<Block />}
+        />
+        <BottomNavigationItem
+          label='Mark All Complete'
+          icon={<DoneAll />}
+        />
+        <ToolbarSeparator />
+        <RaisedButton label='Create Plate' onTouchTap={openDialog} primary />
+      </ToolbarGroup>
+    </Toolbar>
     <AddPlateDialog
       open={open}
       openDialog={openDialog}
       closeDialog={closeDialog}
     />
-  </Paper>
+  </div>
 )
