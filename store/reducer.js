f1cd527f7516bdf9474ef02f0d4114cc80f27de2
knipferrc <knipferrc@gmail.com>
2017-03-10 14:12:15 -0600

    add redux-form

--- store/reducer.js
+++ store/reducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 import dashboard from "./modules/dashboard";
 import drawer from "./modules/drawer";
+import { reducer as formReducer } from "redux-form";
 
 // Root reducer for our redux store and
 // apollo's store
@@ -8,6 +9,7 @@ export default function getReducer(client) {
   return combineReducers({
     drawer,
     dashboard,
+    form: formReducer,
     apollo: client.reducer()
   });
 }
