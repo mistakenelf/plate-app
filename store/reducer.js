a7e116f148a60d784ed565a5b560f22d704b660b
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-03-10 12:31:18 -0600

    removed footer reducer

--- store/reducer.js
+++ store/reducer.js
@@ -1,7 +1,6 @@
 import { combineReducers } from "redux";
 import dashboard from "./modules/dashboard";
 import drawer from "./modules/drawer";
-import footer from "./modules/footer";
 
 // Root reducer for our redux store and
 // apollo's store
@@ -9,7 +8,6 @@ export default function getReducer(client) {
   return combineReducers({
     drawer,
     dashboard,
-    footer,
     apollo: client.reducer()
   });
 }
