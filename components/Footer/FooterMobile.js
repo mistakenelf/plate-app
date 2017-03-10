4932b15991d0402eae95e290333ff4f4894d04b0
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-03-09 23:33:36 -0600

    updated mobile nav menu

--- components/Footer/FooterMobile.js
+++ components/Footer/FooterMobile.js
@@ -1,16 +1,53 @@
 import { List, ListItem } from "material-ui/List";
 
 import Divider from "material-ui/Divider";
+import ExpandMoreIcon from "material-ui/svg-icons/navigation/expand-more";
 import React from "react";
 
 export default () => (
   <div className="footer-mobile-main">
     <List>
-      <ListItem primaryText="Quick Links" style={{ color: "#bdbdbd" }} />
+      <ListItem
+        primaryText="Quick Links"
+        style={{ color: "#bdbdbd" }}
+        rightIcon={<ExpandMoreIcon />}
+        primaryTogglesNestedList
+        nestedItems={[
+          (
+            <ListItem
+              key={1}
+              primaryText="Register"
+              style={{ color: "#bdbdbd" }}
+            />
+          ),
+          <ListItem key={2} primaryText="Login" style={{ color: "#bdbdbd" }} />
+        ]}
+      />
       <div className="divider-style">
         <Divider />
       </div>
-      <ListItem primaryText="Contact Us" style={{ color: "#bdbdbd" }} />
+      <ListItem
+        primaryText="About Us"
+        style={{ color: "#bdbdbd" }}
+        rightIcon={<ExpandMoreIcon />}
+        primaryTogglesNestedList
+        nestedItems={[
+          (
+            <ListItem
+              key={1}
+              primaryText="Register"
+              style={{ color: "#bdbdbd" }}
+            />
+          ),
+          (
+            <ListItem
+              key={2}
+              primaryText="Contact Us"
+              style={{ color: "#bdbdbd" }}
+            />
+          )
+        ]}
+      />
     </List>
     <style jsx>
       {
@@ -19,8 +56,12 @@ export default () => (
           background-color: #1E2532;
           margin: 30px;
         }
+
+        .footer-mobile-main svg {
+          color: white;
+        }
         .divider-style {
-          opacity: 0.2;
+          opacity: 0.1;
           margin-top: 5px;
           margin-bottom: 5px;
         }
