960c23c96dda6581f6a21c072cc6cc23e0759621
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-03-09 22:56:29 -0600

    add footer to root reducer

--- store/reducer.js
+++ store/reducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 import dashboard from "./modules/dashboard";
 import drawer from "./modules/drawer";
+import footer from "./modules/footer";
 
 // Root reducer for our redux store and
 // apollo's store
@@ -8,6 +9,7 @@ export default function getReducer(client) {
   return combineReducers({
     drawer,
     dashboard,
+    footer,
     apollo: client.reducer()
   });
 }
