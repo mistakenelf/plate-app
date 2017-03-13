7f0c57992aab9f278daa7a6550e9276b8ddb376e
knipferrc <knipferrc@gmail.com>
2017-03-13 06:51:28 -0500

    message change

--- forms/RegisterForm/RegisterForm.js
+++ forms/RegisterForm/RegisterForm.js
@@ -18,10 +18,10 @@ const register = () => {
 const validate = values => {
   const errors = {};
   if (!values.email) {
-    errors.email = "Required";
+    errors.email = "Please enter an email";
   }
   if (!values.password) {
-    errors.password = "Required";
+    errors.password = "Please enter a password";
   } else if (
     values.email &&
     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
