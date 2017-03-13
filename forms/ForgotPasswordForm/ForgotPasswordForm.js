1fcf2356abf81354c75b8576b2d5ec39f922d1b4
knipferrc <knipferrc@gmail.com>
2017-03-13 07:48:00 -0500

    validate forms

--- forms/ForgotPasswordForm/ForgotPasswordForm.js
+++ forms/ForgotPasswordForm/ForgotPasswordForm.js
@@ -1,30 +1,66 @@
+// @flow
+
+import { Field, reduxForm } from "redux-form";
+
 import BorderedButton from "../../components/BorderedButton/BorderedButton";
 import React from "react";
 import TextField from "material-ui/TextField";
+
+type Props = {
+  handleSubmit?: Function,
+  label?: string,
+  meta?: Object,
+  input?: Object
+};
 
 const forgotPassword = e => {
   e.preventDefault();
   console.log("submitted");
 };
 
-export default () => (
+const validate = values => {
+  const errors = {};
+  if (!values.email) {
+    errors.email = "Please enter an email";
+  } else if (
+    values.email &&
+    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+  ) {
+    errors.email = "Invalid email address";
+  }
+  return errors;
+};
+
+const renderTextField = (
+  { input, label, meta: { touched, error }, ...custom }: Props
+) => (
+  <TextField
+    hintStyle={{ color: "white" }}
+    inputStyle={{ color: "white" }}
+    hintText={label}
+    autoComplete={"off"}
+    errorText={touched && error}
+    fullWidth
+    {...input}
+    {...custom}
+  />
+);
+
+const ForgotPasswordForm = ({ handleSubmit }: Props) => (
   <div className="container-fluid">
     <div
       className="row full-height middle-xs middle-sm middle-md middle-lg middle-xl center-xs center-sm center-md center-lg center-lg"
     >
       <div className="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
         <h1 className="header-text">Forgot Password</h1>
-        <form onSubmit={forgotPassword}>
+        <form onSubmit={handleSubmit(forgotPassword)}>
           <div className="text-field-email">
-            <TextField
-              hintText="email"
+            <Field
+              name="email"
               id="email"
-              hintStyle={{ color: "white" }}
-              type="text"
-              inputStyle={{ color: "white" }}
-              autoComplete={"off"}
-              fullWidth
-              required
+              component={renderTextField}
+              type="email"
+              label="Email"
             />
           </div>
           <BorderedButton type="submit" label="Forgot Password" />
@@ -49,3 +85,8 @@ export default () => (
     </style>
   </div>
 );
+
+export default reduxForm({
+  form: "forgotPasswordForm",
+  validate
+})(ForgotPasswordForm);
