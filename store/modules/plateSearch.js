6cae66b806a9efd1f2a3192fa9b6632eb5ffab8e
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-03-16 00:50:37 -0500

    add plate search actions and reducer

---
+++ store/modules/plateSearch.js
@@ -0,0 +1,32 @@
+// Constants
+export const constants = {
+  SEARCH_TEXT: "Dashboard/SEARCH_TEXT"
+};
+
+// Actions
+export const actions = {
+  doSearch(searchText) {
+    return {
+      type: constants.SEARCH_TEXT,
+      searchText: searchText
+    };
+  }
+};
+
+// Initial State
+export const initialState = {
+  searchText: null
+};
+
+// Reducer
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case constants.SEARCH_TEXT:
+      return {
+        ...state,
+        searchText: action.searchText
+      };
+    default:
+      return state;
+  }
+};
--- store/reducer.js
+++ store/reducer.js
@@ -1,12 +1,14 @@
 import { combineReducers } from "redux";
 import drawer from "./modules/drawer";
 import { reducer as formReducer } from "redux-form";
+import plateSearch from "./modules/plateSearch";
 
 // Root reducer for our redux store and
 // apollo's store
 export default function getReducer(client) {
   return combineReducers({
     drawer,
+    plateSearch,
     form: formReducer,
     apollo: client.reducer()
   });
