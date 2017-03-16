e547527d7e5c68c91cb69a8597ab8dcb0c0eaa2f
knipferrc <knipferrc@gmail.com>
2017-03-16 10:36:04 -0500

    add header

--- modules/dashboard/index.js
+++ modules/dashboard/index.js
@@ -1,9 +1,11 @@
 import DashboardContainer from "./containers/DashboardContainer";
 import DefaultLayout from "../../layouts/DefaultLayout/DefaultLayout";
+import Header from "../../components/Header/Header";
 import React from "react";
 
 export default () => (
   <DefaultLayout>
+    <Header title="Dashboard" />
     <DashboardContainer />
   </DefaultLayout>
 );
