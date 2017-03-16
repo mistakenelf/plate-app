45c88fdc988d8e5ee1f3d2d17f1f85947687fa82
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-03-16 01:19:18 -0500

    only render all plates if search field is empty

--- components/Dashboard/DashboardView.js
+++ components/Dashboard/DashboardView.js
@@ -30,23 +30,24 @@ const DashboardView = (
       </div>
     </div>
     <div className="row">
-      {allPlates.map((plate, index) => {
-        return (
-          <div
-            key={index}
-            className="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-4"
-            style={{ marginBottom: 10 }}
-          >
-            <Plate
-              plateId={plate.id}
-              name={plate.name}
-              description={plate.description}
-              removePlate={removePlate}
-              refetch={refetch}
-            />
-          </div>
-        );
-      })}
+      {searchText === null &&
+        allPlates.map((plate, index) => {
+          return (
+            <div
+              key={index}
+              className="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-4"
+              style={{ marginBottom: 10 }}
+            >
+              <Plate
+                plateId={plate.id}
+                name={plate.name}
+                description={plate.description}
+                removePlate={removePlate}
+                refetch={refetch}
+              />
+            </div>
+          );
+        })}
     </div>
   </div>
 );
