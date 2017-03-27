c2c20246557a8f49b5e0372cb288c7a624debbb8
knipferrc <knipferrc@gmail.com>
2017-03-27 12:49:49 -0500

    animation component

---
+++ components/Animation/Animation.js
@@ -0,0 +1,59 @@
+import React, { PropTypes } from "react";
+
+import ReactCSSTransitionGroup from "react-addons-css-transition-group";
+
+const Animation = props => {
+  const { children } = props;
+
+  return (
+    <div>
+      <ReactCSSTransitionGroup
+        transitionName="example"
+        transitionAppear
+        transitionEnterTimeout={500}
+        transitionLeaveTimeout={300}
+        transitionAppearTimeout={1500}
+      >
+
+        {children}
+      </ReactCSSTransitionGroup>
+      <style jsx global>
+        {
+          `
+          .example-enter {
+            transform: translate3d(100%, 0, 0);
+          }
+
+          .example-enter.example-enter-active {
+            transform: translate3d(0, 0, 0);
+            transition: all 600ms;
+          }
+
+          .example-leave {
+            opacity: 1;
+          }
+
+          .example-leave.example-leave-active {
+            opacity: 0.01;
+            transition: opacity 300ms ease-in;
+          }
+          .example-appear {
+            transform: translate3d(100%, 0, 0);
+          }
+
+          .example-appear.example-appear-active {
+            transform: translate3d(0, 0, 0);
+            transition: all 600ms;
+          }
+          `
+        }
+      </style>
+    </div>
+  );
+};
+
+Animation.propTypes = {
+  children: PropTypes.node
+};
+
+export default Animation;
