4c4acda0a1483c587eabd4951363c6b9cf0d240a
knipferrc <knipferrc@gmail.com>
2017-03-28 13:48:24 -0500

    confirm delete

--- modules/Dashboard/components/Plate.js
+++ modules/Dashboard/components/Plate.js
@@ -5,68 +5,113 @@ import {
   CardText,
   CardTitle,
 } from "material-ui/Card";
-import React, { PropTypes } from "react";
+import React, { Component, PropTypes } from "react";
 
+import Dialog from "material-ui/Dialog";
 import FlatButton from "material-ui/FlatButton";
 import Link from "next/link";
 
-const markPlateComplete = (plateId, completed, completePlate) => {
-  completePlate(plateId, !completed);
-};
+export default class Plate extends Component {
+  static propTypes = {
+    name: PropTypes.string,
+    description: PropTypes.string,
+    plateId: PropTypes.string,
+    removePlate: PropTypes.func,
+    cardImage: PropTypes.string,
+    completed: PropTypes.bool,
+    completePlate: PropTypes.func
+  };
 
-const deletePlate = (removePlate, plateId) => {
-  removePlate(plateId);
-};
+  state = {
+    open: false
+  };
 
-const Plate = props => {
-  return (
-    <div className="card">
-      <Card
-        style={{
-          borderRadius: 5
-        }}
-      >
-        <CardMedia
-          overlay={<CardTitle title={props.name} />}
-          onTouchTap={() =>
-            markPlateComplete(
-              props.plateId,
-              props.completed,
-              props.completePlate
-            )}
+  handleOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  deletePlate = (removePlate, plateId) => {
+    removePlate(plateId);
+  };
+
+  markPlateComplete = (plateId, completed, completePlate) => {
+    completePlate(plateId, !completed);
+  };
+
+  render() {
+    const actions = [
+      <FlatButton label="Cancel" primary onTouchTap={this.handleClose} />,
+      <FlatButton
+        label="Discard"
+        secondary
+        onTouchTap={() =>
+          this.deletePlate(this.props.removePlate, this.props.plateId)}
+      />
+    ];
+    return (
+      <div className="card">
+        <Card
+          style={{
+            borderRadius: 5
+          }}
         >
-          <img
-            style={{ borderTopRightRadius: 5, borderTopLeftRadius: 5 }}
-            src={props.cardImage}
-            width="208"
-            height="214"
-          />
-        </CardMedia>
-        <CardText>
-          <div style={{ marginBottom: 10 }}>
-            {props.description}
-          </div>
-          <div>
-            Status: {props.completed ? "Closed" : "Open"}
-          </div>
-        </CardText>
-        <CardActions>
-          <FlatButton
-            label="Wash Plate"
-            secondary
-            onTouchTap={() => deletePlate(props.removePlate, props.plateId)}
-          />
-          <Link
-            prefetch
-            href={{ pathname: "platefiller", query: { id: props.plateId } }}
+          <CardMedia
+            overlay={<CardTitle title={this.props.name} />}
+            onTouchTap={() =>
+              this.markPlateComplete(
+                this.props.plateId,
+                this.props.completed,
+                this.props.completePlate
+              )}
           >
-            <a><FlatButton primary label="Fill Plate" /></a>
-          </Link>
-        </CardActions>
-      </Card>
-      <style jsx>
-        {
-          `
+            <img
+              style={{ borderTopRightRadius: 5, borderTopLeftRadius: 5 }}
+              src={this.props.cardImage}
+              width="208"
+              height="214"
+            />
+          </CardMedia>
+          <CardText>
+            <div style={{ marginBottom: 10 }}>
+              {this.props.description}
+            </div>
+            <div>
+              Status: {this.props.completed ? "Closed" : "Open"}
+            </div>
+          </CardText>
+          <CardActions>
+            <FlatButton
+              label="Wash Plate"
+              secondary
+              onTouchTap={this.handleOpen}
+            />
+            <Link
+              prefetch
+              href={{
+                pathname: "platefiller",
+                query: { id: this.props.plateId }
+              }}
+            >
+              <a><FlatButton primary label="Fill Plate" /></a>
+            </Link>
+          </CardActions>
+          <Dialog
+            actions={actions}
+            modal={false}
+            open={this.state.open}
+            onRequestClose={this.handleClose}
+            contentStyle={{ width: "95%" }}
+          >
+            Are you sure you want to remove this plate?
+          </Dialog>
+        </Card>
+        <style jsx>
+          {
+            `
           .card {
             transition: 0.3s;
           }
@@ -75,20 +120,9 @@ const Plate = props => {
             box-shadow: 0 3px 8px 0 rgba(0,0,0,0.2);
           }
           `
-        }
-      </style>
-    </div>
-  );
-};
-
-Plate.propTypes = {
-  name: PropTypes.string,
-  description: PropTypes.string,
-  plateId: PropTypes.string,
-  removePlate: PropTypes.func,
-  cardImage: PropTypes.string,
-  completed: PropTypes.bool,
-  completePlate: PropTypes.func
-};
-
-export default Plate;
+          }
+        </style>
+      </div>
+    );
+  }
+}
