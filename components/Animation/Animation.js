58f7eba182486a7f2e5fd8ea08da5663992ca5b6
knipferrc <knipferrc@gmail.com>
2017-03-29 10:37:14 -0500

    use class

--- components/Animation/Animation.js
+++ components/Animation/Animation.js
@@ -1,40 +1,38 @@
-import React, { PropTypes } from "react";
+import React, { Component, PropTypes } from "react";
 
 import ReactCSSTransitionGroup from "react-addons-css-transition-group";
 
-const Animation = props => {
-  return (
-    <div>
-      <ReactCSSTransitionGroup
-        transitionName="example"
-        transitionAppear={true}
-        transitionAppearTimeout={500}
-        transitionEnter={false}
-        transitionLeave={false}
-      >
+export default class Animation extends Component {
+  static propTypes = {
+    children: PropTypes.node
+  };
 
-        {props.children}
-      </ReactCSSTransitionGroup>
-      <style jsx global>
-        {
-          `
-          .example-appear {
-            transform: translate3d(100%, 0, 0);
+  render() {
+    return (
+      <div>
+        <ReactCSSTransitionGroup
+          transitionName="example"
+          transitionAppear={true}
+          transitionAppearTimeout={200}
+          transitionLeave={false}
+          transitionEnter={false}
+        >
+          {this.props.children}
+        </ReactCSSTransitionGroup>
+        <style jsx global>
+          {
+            `
+            .example-appear {
+              opacity: 0.01;
+            }
+            .example-appear.example-appear-active {
+              opacity: 1;
+              transition: opacity 1s ease-in;
+            }
+            `
           }
-
-          .example-appear.example-appear-active {
-            transform: translate3d(0, 0, 0);
-            transition: all 300ms;
-          }
-          `
-        }
-      </style>
-    </div>
-  );
-};
-
-Animation.propTypes = {
-  children: PropTypes.node
-};
-
-export default Animation;
+        </style>
+      </div>
+    );
+  }
+}
