9ab4a0dcf38a16d113636a192dbc9f10beac9a85
knipferrc <knipferrc@gmail.com>
2017-03-29 13:37:56 -0500

    not needed

--- modules/NavlessLayout/index.js
+++ modules/NavlessLayout/index.js
@@ -3,20 +3,6 @@ import React, { PropTypes } from "react";
 const NavlessLayout = props => {
   return (
     <div>
-      <style jsx global>
-        {
-          `
-          * {
-            margin: 0;
-            box-sizing: border-box;
-          }
-          body {
-            background: linear-gradient(-90deg, rgb(141, 95, 115), #343f53);
-            font-family: 'Fira Sans', sans-serif;
-          }
-        `
-        }
-      </style>
       {props.children}
     </div>
   );
