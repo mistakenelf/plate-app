af5cb97cc411eb2c751a6ecff707b6f2e538b46e
knipferrc <knipferrc@gmail.com>
2017-03-30 07:15:59 -0500

    image fixes

--- modules/Home/components/HomeDetails.js
+++ modules/Home/components/HomeDetails.js
@@ -3,8 +3,10 @@ import React from "react";
 const HomeDetails = () => {
   return (
     <div className="home-details">
-      <div className="initial-row row full-height">
-        <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+      <div className="row">
+        <div
+          className="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 text-spacing"
+        >
           <h2>Easily create tasks</h2><br />
           <p>
             {" "}
@@ -12,34 +14,44 @@ const HomeDetails = () => {
             Our interface provides the most simplistic way for creatings tasks
           </p>
         </div>
-        <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+        <div
+          className="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 img-spacing"
+        >
           <img src="/static/img/taskIcon/taskIconBlue.png" alt="Blue Task" />
         </div>
       </div>
-      <div className="initial-row row full-height">
-        <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+      <div className="row">
+        <div
+          className="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 text-spacing"
+        >
           <h2>Easily create tasks</h2><br />
           <p>
             Create and manage tasks without any pain.<br />
             Our interface provides the most simplistic way for creatings tasks
           </p>
         </div>
-        <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6col-xl-6">
+        <div
+          className="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 img-spacing"
+        >
           <img
             src="/static/img/taskIcon/taskIconOrange.png"
             alt="Orange Task"
           />
         </div>
       </div>
-      <div className="initial-row row full-height">
-        <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+      <div className="row">
+        <div
+          className="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 text-spacing"
+        >
           <h2>Easily create tasks</h2><br />
           <p>
             Create and manage tasks without any pain.<br />
             Our interface provides the most simplistic way for creatings tasks
           </p>
         </div>
-        <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6col-xl-6">
+        <div
+          className="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 img-spacing"
+        >
           <img src="/static/img/taskIcon/taskIconGreen.png" alt="Green Task" />
         </div>
       </div>
@@ -49,29 +61,16 @@ const HomeDetails = () => {
           .home-details {
             padding: 80px;
           }
-          .initial-row {
-            height: 300px;
-            margin-bottom: 50px;
-          }
-          h2 {
-            font-weight: normal;
-          }
           img {
             width: 100%;
             height: 100%;
           }
-          @media only screen
-            and (min-device-width : 1080px) {
-              .initial-row {
-                margin-bottom: 350px;
-              }
-            }
-            @media only screen
-            and (min-device-width : 768px) {
-              .initial-row {
-                margin-bottom: 150px;
-              }
-            }
+          .img-spacing {
+            margin-bottom: 30px;
+          }
+          .text-spacing {
+            margin-bottom: 10px;
+          }
           `
         }
       </style>
