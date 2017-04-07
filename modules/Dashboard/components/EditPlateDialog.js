57549df63aefc3fec527fd0a3e3559a9ec2fa091
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-04-07 10:04:08 -0500

    convert to class

--- modules/Dashboard/components/EditPlateDialog.js
+++ modules/Dashboard/components/EditPlateDialog.js
@@ -1,67 +1,69 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
 import TextField from 'material-ui/TextField';
 
-const editPlateDetails = (id, editPlate, editPlateHandleClose) => {
-  const newPlateName = document.getElementById('currentPlateName').value;
-  const newPlateDescription = document.getElementById(
-    'currentPlateDescription'
-  ).value;
+class EditPlateDialog extends Component {
+  static propTypes = {
+    editPlateOpen: PropTypes.bool,
+    editPlateHandleClose: PropTypes.func,
+    plateId: PropTypes.string,
+    plateName: PropTypes.string,
+    plateDescription: PropTypes.string,
+    editPlate: PropTypes.func
+  };
 
-  editPlate(id, newPlateName, newPlateDescription);
-  editPlateHandleClose();
-};
+  editPlateDetails = (id, editPlate, editPlateHandleClose) => {
+    const newPlateName = document.getElementById('currentPlateName').value;
+    const newPlateDescription = document.getElementById(
+      'currentPlateDescription'
+    ).value;
 
-const EditPlateDialog = props => {
-  const actions = [
-    <FlatButton
-      label="Cancel"
-      onTouchTap={props.editPlateHandleClose}
-      primary
-    />,
-    <FlatButton
-      label="Submit"
-      secondary
-      onTouchTap={() =>
-        editPlateDetails(
-          props.plateId,
-          props.editPlate,
-          props.editPlateHandleClose
-        )}
-    />
-  ];
+    editPlate(id, newPlateName, newPlateDescription);
+    editPlateHandleClose();
+  };
 
-  return (
-    <Dialog
-      actions={actions}
-      modal={false}
-      open={props.editPlateOpen}
-      onRequestClose={props.editPlateHandleClose}
-      contentStyle={{ width: '95%' }}
-    >
-      <h3>Edit Plate</h3>
-      <br />
-      <span style={{ marginRight: 10 }}>Name:</span>
-      <TextField id="currentPlateName" defaultValue={props.plateName} />
-      <br />
-      <span style={{ marginRight: 10 }}>Description:</span>
-      <TextField
-        id="currentPlateDescription"
-        defaultValue={props.plateDescription}
+  render() {
+    const actions = [
+      <FlatButton
+        label="Cancel"
+        onTouchTap={this.props.editPlateHandleClose}
+        primary
+      />,
+      <FlatButton
+        label="Submit"
+        secondary
+        onTouchTap={() =>
+          editPlateDetails(
+            this.props.plateId,
+            this.props.editPlate,
+            this.props.editPlateHandleClose
+          )}
       />
-    </Dialog>
-  );
-};
+    ];
 
-EditPlateDialog.propTypes = {
-  editPlateOpen: PropTypes.bool,
-  editPlateHandleClose: PropTypes.func,
-  plateId: PropTypes.string,
-  plateName: PropTypes.string,
-  plateDescription: PropTypes.string,
-  editPlate: PropTypes.func
-};
+    return (
+      <Dialog
+        actions={actions}
+        modal={false}
+        open={this.props.editPlateOpen}
+        onRequestClose={this.props.editPlateHandleClose}
+        contentStyle={{ width: '95%' }}
+      >
+        <h3>Edit Plate</h3>
+        <br />
+        <span style={{ marginRight: 10 }}>Name:</span>
+        <TextField id="currentPlateName" defaultValue={this.props.plateName} />
+        <br />
+        <span style={{ marginRight: 10 }}>Description:</span>
+        <TextField
+          id="currentPlateDescription"
+          defaultValue={this.props.plateDescription}
+        />
+      </Dialog>
+    );
+  }
+}
 
 export default EditPlateDialog;
