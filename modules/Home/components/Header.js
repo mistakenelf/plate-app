5b22732e99810b073e4b3d0ec6122203410d061b
knipferrc <knipferrc@gmail.com>
2017-04-11 11:46:03 -0500

    slide cleanup

--- modules/Home/components/Header.js
+++ modules/Home/components/Header.js
@@ -1,5 +1,5 @@
-import Banner from './Banner';
 import React from 'react';
+import SlideOne from './SlideOne';
 import SwipeableViews from 'react-swipeable-views';
 import { autoPlay } from 'react-swipeable-views-utils';
 
@@ -8,10 +8,14 @@ const styles = {
     margin: 0,
     padding: 0,
     color: 'white',
-    height: 400
+    height: 400,
+    display: 'flex',
+    justifyContent: 'center',
+    textAlign: 'center',
+    alignItems: 'center'
   },
   slide1: {
-    background: '#FEA900'
+    background: 'linear-gradient(-90deg, rgb(141, 95, 115), #343f53)'
   },
   slide2: {
     background: '#B3DC4A'
@@ -24,9 +28,9 @@ const styles = {
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 const Header = () => (
-  <AutoPlaySwipeableViews enableMouseEvents resistance interval={400000}>
+  <AutoPlaySwipeableViews enableMouseEvents resistance interval={4000}>
     <div style={Object.assign({}, styles.slide, styles.slide1)}>
-      <Banner />
+      <SlideOne />
     </div>
     <div style={Object.assign({}, styles.slide, styles.slide2)}>slide n2</div>
     <div style={Object.assign({}, styles.slide, styles.slide3)}>slide n3</div>
