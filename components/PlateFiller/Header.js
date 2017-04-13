91d5c5dc15e36b62f1276197ee274eb382b4c2db
knipferrc <knipferrc@gmail.com>
2017-04-13 16:26:59 -0500

    cleanup

--- components/PlateFiller/Header.js
+++ components/PlateFiller/Header.js
@@ -5,11 +5,13 @@ const Header = ({ plateName }) => {
   return (
     <div>
       <div className="header">{plateName.toUpperCase()}</div>
-      <hr style={{ marginBottom: 15 }} />
+      <hr style={{ marginBottom: 20 }} />
       <style jsx>
         {`
             .header {
               font-size: 45px;
+              margin-top: 15px;
+              margin-bottom: 15px;
             }
           `}
       </style>
