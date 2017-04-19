53296226b640ee66d820e9c3f131421e6e9012f3
knipferrc <knipferrc@gmail.com>
2017-04-19 10:59:33 -0500

    remove auth requirement

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,10 @@ import Header from '../components/Header/Header'
 import HomeContainer from '../containers/HomeContainer'
 import React from 'react'
 import page from '../hocs/page'
-import withAuth from '../hocs/withAuth'
 
-export default page(
-  withAuth(() => (
-    <div>
-      <Header title="Home" />
-      <HomeContainer />
-    </div>
-  ))
-)
+export default page(() => (
+  <div>
+    <Header title="Home" />
+    <HomeContainer />
+  </div>
+))
