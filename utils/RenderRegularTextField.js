dbbe57f96fa19bedc9b810f8d91263e6f6b5647d
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-04-20 21:51:56 -0500

    fix default values for editing plate

--- utils/RenderRegularTextField.js
+++ utils/RenderRegularTextField.js
@@ -4,7 +4,7 @@ import TextField from 'material-ui/TextField'
 
 const RenderRegularTextField = props => {
   const {
-    input: { ...input },
+    input: { value, ...input },
     label,
     meta: { touched, error },
     ...custom
