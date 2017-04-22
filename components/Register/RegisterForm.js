98793d757ca1999631cf08bc7148d1904e9f1497
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-04-21 23:26:01 -0500

    registration error display

--- components/Register/RegisterForm.js
+++ components/Register/RegisterForm.js
@@ -1,72 +1,105 @@
+import { Card, CardText } from 'material-ui/Card'
 import { Field, reduxForm } from 'redux-form'
+import React, { Component } from 'react'
 import { compose, graphql } from 'react-apollo'
 
 import BorderedButton from '../BorderedButton/BorderedButton'
 import PropTypes from 'prop-types'
-import React from 'react'
 import RenderWhiteTextField from '../../utils/RenderWhiteTextField'
 import cookie from 'react-cookie'
 import { registerMutation } from '../../mutations/registerMutations'
 import { registerValidations } from '../../validations/registerValidations'
 
-const RegisterForm = ({ handleSubmit, register }) => {
-  const registerUser = async () => {
+class RegisterForm extends Component {
+  static propTypes = {
+    handleSubmit: PropTypes.func,
+    register: PropTypes.func
+  }
+
+  state = {
+    registerError: false
+  }
+
+  registerUser = async () => {
     const firstName = document.getElementById('firstName').value
     const lastName = document.getElementById('lastName').value
     const username = document.getElementById('username').value
     const password = document.getElementById('password').value
 
-    const user = await register(firstName, lastName, username, password)
+    const user = await this.props.register(
+      firstName,
+      lastName,
+      username,
+      password
+    )
 
     if (user.data.register === null) {
-      alert('Registration error')
+      this.setState({
+        registerError: true
+      })
     } else {
       cookie.save('token', user.data.register, { path: '/' })
       window.location.href = '/'
     }
   }
 
-  return (
-    <div className="container-fluid">
-      <div className="row full-height middle-xs middle-sm middle-md middle-lg center-xs center-sm center-md center-lg">
-        <div className="col-xs-12 col-sm-12 col-md-6 col-lg-4">
-          <h1 className="header-text">Register</h1>
-          <form onSubmit={handleSubmit(registerUser)}>
-            <Field
-              name="firstName"
-              id="firstName"
-              component={RenderWhiteTextField}
-              type="text"
-              label="First Name"
-            />
-            <Field
-              name="lastName"
-              id="lastName"
-              component={RenderWhiteTextField}
-              type="text"
-              label="Last Name"
-            />
-            <Field
-              name="username"
-              id="username"
-              component={RenderWhiteTextField}
-              type="text"
-              label="Username"
-            />
-            <Field
-              name="password"
-              id="password"
-              component={RenderWhiteTextField}
-              type="password"
-              label="Password"
-              style={{ marginBottom: 20 }}
-            />
-            <BorderedButton color="white" type="submit" label="Register" />
-          </form>
+  render() {
+    const { handleSubmit } = this.props
+
+    const errorStyle = {
+      marginBottom: 10,
+      backgroundColor: '#FFCDD2'
+    }
+
+    return (
+      <div className="container-fluid">
+        <div className="row full-height middle-xs middle-sm middle-md middle-lg center-xs center-sm center-md center-lg">
+          <div className="col-xs-12 col-sm-12 col-md-6 col-lg-4">
+            <h1 className="header-text">Register</h1>
+            {this.state.registerError &&
+              <Card style={errorStyle}>
+                <div className="error-text">
+                  <CardText>
+                    <b>Error:</b> Registration Error.
+                  </CardText>
+                </div>
+              </Card>}
+            <form onSubmit={handleSubmit(this.registerUser)}>
+              <Field
+                name="firstName"
+                id="firstName"
+                component={RenderWhiteTextField}
+                type="text"
+                label="First Name"
+              />
+              <Field
+                name="lastName"
+                id="lastName"
+                component={RenderWhiteTextField}
+                type="text"
+                label="Last Name"
+              />
+              <Field
+                name="username"
+                id="username"
+                component={RenderWhiteTextField}
+                type="text"
+                label="Username"
+              />
+              <Field
+                name="password"
+                id="password"
+                component={RenderWhiteTextField}
+                type="password"
+                label="Password"
+                style={{ marginBottom: 20 }}
+              />
+              <BorderedButton color="white" type="submit" label="Register" />
+            </form>
+          </div>
         </div>
-      </div>
-      <style jsx>
-        {`
+        <style jsx>
+          {`
           .full-height {
             height: 90vh;
           }
@@ -80,15 +113,14 @@ const RegisterForm = ({ handleSubmit, register }) => {
             color: white;
             margin-bottom: 50px;
           }
+          .error-text {
+            text-align: center;
+          }
           `}
-      </style>
-    </div>
-  )
-}
-
-RegisterForm.propTypes = {
-  handleSubmit: PropTypes.func,
-  register: PropTypes.func
+        </style>
+      </div>
+    )
+  }
 }
 
 export default compose(
