403eb772f8b5578bc3abccbe691b616a2a678ac5
knipferrc <knipferrc@gmail.com>
2017-04-24 14:06:29 -0500

    just triggering a build

--- pages/team.js
+++ pages/team.js
@@ -2,7 +2,11 @@ import React from 'react'
 import { pageWithDefaultLayout } from '../hocs/page'
 
 const Team = () => {
-  return <h1>Team</h1>
+  return (
+    <div>
+      <h1>Team</h1>
+    </div>
+  )
 }
 
 export default pageWithDefaultLayout(Team)
