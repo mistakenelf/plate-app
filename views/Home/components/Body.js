f796be127f10e4ea77123cc16270f5e14a74de72
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-04-27 22:37:17 -0500

    begin creating home page layout and body

--- views/Home/components/Body.js
+++ views/Home/components/Body.js
@@ -3,33 +3,71 @@ import React from 'react'
 const Body = () => {
   return (
     <div className="body">
-      <div className="lead-text">
-        Wash and Create as many plates as you want.
-        Get started now or if you want to learn more get
-        in contact with us and we'll help out.
+      <div className="row section">
+        <div className="align-items col-xs-12 col-sm-12 col-md-6 col-lg-6 center-xs center-sm center-md center-lg center-lg">
+          <img src="/static/img/mobile.png" />
+        </div>
+        <div className="align-items col-xs-12 col-sm-12 col-md-6 col-lg-6">
+          <div className="segment">
+            <h2 className="section-titles">Easily manage your tasks</h2>
+            <p>
+              Manage your tasks easily through an elegant and simple interface.
+              Never forget what's on your list of tasks to be completed.
+            </p>
+          </div>
+        </div>
       </div>
-      <img src="https://res.cloudinary.com/knipferrc/image/upload/v1493208940/plateLogo_wa3wpi.png" />
+      <div className="row section-grey">
+        <div className="align-items one col-xs-12 col-sm-12 col-md-6 col-lg-6">
+          <div className="segment">
+            <h2 className="section-titles">On the go</h2>
+            <p>
+              Edit and view your tasks on all platforms. Our responsive design allows
+              for ease of access on all devices. Big, small, huge!
+            </p>
+          </div>
+        </div>
+        <div className="align-items two col-xs-12 col-sm-12 col-md-6 col-lg-6 center-xs center-sm center-md center-lg center-lg">
+          <img src="/static/img/mobile.png" />
+        </div>
+      </div>
       <style jsx>
         {`
           .body {
-            display: flex;
             padding: 5px;
-            color: white;
-            align-items: center;
-            font-size: 20px;
-            height: 300px;
-            flex-direction: column;
-            justify-content: center;
-            background-color: rgb(255, 64, 129);
+            height: auto;
+          }
+          .section {
+            width: auto;
+            height: 350px;
+          }
+          .section-grey {
+            width: auto;
+            height: 350px;
+            background-color: #FAFAFA;
+          }
+
+          .align-items {
+            margin-top: auto;
+            margin-bottom: auto;
           }
           img {
-            height: 100px;
-            width: 100px;
+            padding: 10px;
           }
-          .lead-text {
-            text-align: center;
-            font-size: 20px;
-            margin-bottom: 15px;
+          .section-titles {
+            color: #172E4F;
+            margin-bottom: 10px;
+          }
+          .segment {
+            width: 300px;
+            margin-left: auto;
+            margin-right: auto;
+          }
+          @media only screen
+          and (min-device-width : 320px)
+          and (max-device-width : 1030px) {
+            .one{order: 1}
+            .two{order: 0}
           }
         `}
       </style>
