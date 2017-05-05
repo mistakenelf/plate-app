d610b5cb63ce7225285e9c00c631075b70f21783
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-05-04 22:46:35 -0500

    add test modal to meet the team page

--- views/MeetTheTeam/index.js
+++ views/MeetTheTeam/index.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const MeetTheTeam = () => {
-  return <h1>Meet The Team</h1>
+import RaisedButton from 'material-ui/RaisedButton'
+import TestModal from '../../components/TestModal/TestModal'
+
+class MeetTheTeam extends Component {
+  state = {
+    displayModal: false
+  }
+
+  openModal = () => {
+    this.setState({
+      displayModal: true
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <RaisedButton label="Primary" primary onTouchTap={this.openModal} />
+        {this.state.displayModal && <TestModal />}
+      </div>
+    )
+  }
 }
 
 export default MeetTheTeam
