181b5f9f3bbfd2095f66e9ba58ec05c5edc5dd46
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-05-04 23:52:56 -0500

    reducer / actions for modal

--- store/reducer.js
+++ store/reducer.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
+import modalReducer from '../redux/modalReducer'
 
 // Root reducer for our redux store and
 // apollo's store
 const getReducer = client => {
   return combineReducers({
     form: formReducer,
-    apollo: client.reducer()
+    apollo: client.reducer(),
+    modalReducer
   })
 }
 
