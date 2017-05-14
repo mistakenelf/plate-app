e232f51d36795635cf28355bce4dd0b6beae6ada
knipferrc <knipferrc@gmail.com>
2017-05-14 14:05:48 -0500

    basic about us page
    Signed-off-by: knipferrc <knipferrc@gmail.com>

--- views/About/index.js
+++ views/About/index.js
@@ -1,9 +1,15 @@
+import PageHeader from '../../components/PageHeader/PageHeader'
 import React from 'react'
 
 const About = () => {
   return (
     <div className="container">
-      <h1>About</h1>
+      <PageHeader headerText="About Us" />
+      <p>
+        We are a team of two developers, also brothers, who are determined to make task management
+        easier, and more fun to do. It was built as a learning experience for us, but we realized that
+        maybe other would want to use it as well, so Plate was born.
+      </p>
     </div>
   )
 }
