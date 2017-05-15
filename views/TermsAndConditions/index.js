fd3daa31e6475c2a8d4af1e190ecd1a4cd0bb3f6
knipferrc <knipferrc@gmail.com>
2017-05-15 06:44:35 -0500

    fix build
    Signed-off-by: knipferrc <knipferrc@gmail.com>

--- views/TermsAndConditions/index.js
+++ views/TermsAndConditions/index.js
@@ -7,27 +7,27 @@ const TermsAndConditions = () => {
       <PageHeader headerText="Plate Terms of Service and Privacy Policy" />
       <h3>1. Terms</h3>
       <p>
-        By accessing the website at
+        By accessing the website at &nbsp;
         <a href="https://platemate.io">https://platemate.io</a>
         , you are agreeing to be bound by these terms of service, all applicable laws and regulations, and agree that you are responsible for compliance with any applicable local laws. If you do not agree with any of these terms, you are prohibited from using or accessing this site. The materials contained in this website are protected by applicable copyright and trademark law.
       </p>
       <h3>2. Use License</h3>
       <ol type="a">
         <li>
-          Permission is granted to temporarily download one copy of the materials (information or software) on Plate's website for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title, and under this license you may not:
+          Permission is granted to temporarily download one copy of the materials (information or software) on Plates website for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title, and under this license you may not:
           <ol type="i">
             <li>modify or copy the materials;</li>
             <li>
               use the materials for any commercial purpose, or for any public display (commercial or non-commercial);
             </li>
             <li>
-              attempt to decompile or reverse engineer any software contained on Plate's website;
+              attempt to decompile or reverse engineer any software contained on Plates website;
             </li>
             <li>
               remove any copyright or other proprietary notations from the materials; or
             </li>
             <li>
-              transfer the materials to another person or "mirror" the materials on any other server.
+              transfer the materials to another person or mirror the materials on any other server.
             </li>
           </ol>
         </li>
@@ -38,7 +38,7 @@ const TermsAndConditions = () => {
       <h3>3. Disclaimer</h3>
       <ol type="a">
         <li>
-          The materials on Plate's website are provided on an 'as is' basis. Plate makes no warranties, expressed or implied, and hereby disclaims and negates all other warranties including, without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights.
+          The materials on Plates website are provided on an as is basis. Plate makes no warranties, expressed or implied, and hereby disclaims and negates all other warranties including, without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights.
         </li>
         <li>
           Further, Plate does not warrant or make any representations concerning the accuracy, likely results, or reliability of the use of the materials on its website or otherwise relating to such materials or on any sites linked to this site.
@@ -46,15 +46,15 @@ const TermsAndConditions = () => {
       </ol>
       <h3>4. Limitations</h3>
       <p>
-        In no event shall Plate or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption) arising out of the use or inability to use the materials on Plate's website, even if Plate or a Plate authorized representative has been notified orally or in writing of the possibility of such damage. Because some jurisdictions do not allow limitations on implied warranties, or limitations of liability for consequential or incidental damages, these limitations may not apply to you.
+        In no event shall Plate or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption) arising out of the use or inability to use the materials on Plates website, even if Plate or a Plate authorized representative has been notified orally or in writing of the possibility of such damage. Because some jurisdictions do not allow limitations on implied warranties, or limitations of liability for consequential or incidental damages, these limitations may not apply to you.
       </p>
       <h3>5. Accuracy of materials</h3>
       <p>
-        The materials appearing on Plate's website could include technical, typographical, or photographic errors. Plate does not warrant that any of the materials on its website are accurate, complete or current. Plate may make changes to the materials contained on its website at any time without notice. However Plate does not make any commitment to update the materials.
+        The materials appearing on Plates website could include technical, typographical, or photographic errors. Plate does not warrant that any of the materials on its website are accurate, complete or current. Plate may make changes to the materials contained on its website at any time without notice. However Plate does not make any commitment to update the materials.
       </p>
       <h3>6. Links</h3>
       <p>
-        Plate has not reviewed all of the sites linked to its website and is not responsible for the contents of any such linked site. The inclusion of any link does not imply endorsement by Plate of the site. Use of any such linked website is at the user's own risk.
+        Plate has not reviewed all of the sites linked to its website and is not responsible for the contents of any such linked site. The inclusion of any link does not imply endorsement by Plate of the site. Use of any such linked website is at the users own risk.
       </p>
       <h3>7. Modifications</h3>
       <p>
@@ -67,7 +67,7 @@ const TermsAndConditions = () => {
       <h2>Privacy Policy</h2>
       <p>Your privacy is important to us.</p>
       <p>
-        It is Plate's policy to respect your privacy regarding any information we may collect while operating our website. Accordingly, we have developed this privacy policy in order for you to understand how we collect, use, communicate, disclose and otherwise make use of personal information. We have outlined our privacy policy below.
+        It is Plates policy to respect your privacy regarding any information we may collect while operating our website. Accordingly, we have developed this privacy policy in order for you to understand how we collect, use, communicate, disclose and otherwise make use of personal information. We have outlined our privacy policy below.
       </p>
       <ul>
         <li>
@@ -93,7 +93,7 @@ const TermsAndConditions = () => {
         </li>
       </ul>
       <p>
-        We are committed to conducting our business in accordance with these principles in order to ensure that the confidentiality of personal information is protected and maintained. Plate may change this privacy policy from time to time at Plate's sole discretion.
+        We are committed to conducting our business in accordance with these principles in order to ensure that the confidentiality of personal information is protected and maintained. Plate may change this privacy policy from time to time at Plates sole discretion.
       </p>
     </div>
   )
