c293a4d837198eeac393a2d79fc521464dc8883e
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-05-16 00:19:16 -0500

    recover plate mutation

---
+++ mutations/RecoverPlateMutation.js
@@ -0,0 +1,15 @@
+import { gql } from 'react-apollo'
+
+export default gql`
+  mutation recoverPlate($id: ID, $name: String!, $description: String!, $thumbnail: String!, $status: String!, $content: String $createdBy: String!) {
+    recoverPlate(id: $id, name: $name, description: $description, thumbnail: $thumbnail, status: $status, createdBy: $createdBy) {
+      id
+      name
+      __typename
+      description
+      thumbnail
+      status
+      content
+    }
+  }
+`
