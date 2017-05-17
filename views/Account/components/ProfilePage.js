1861b7263c8fbc8fcff9c63aaa3d73ba353968a0
knipferrc <knipferrc@gmail.com>
2017-05-17 17:25:08 -0500

    setting up autosave
    Signed-off-by: knipferrc <knipferrc@gmail.com>

--- views/Account/components/ProfilePage.js
+++ views/Account/components/ProfilePage.js
@@ -1,3 +1,5 @@
+import { injectState, provideState } from 'freactal'
+
 import Cookies from 'js-cookie'
 import PropTypes from 'prop-types'
 import React from 'react'
@@ -5,76 +7,90 @@ import UpdateProfileMutation from '../../../mutations/UpdateProfileMutation'
 import UserProfileQuery from '../../../queries/GetUserProfileQuery'
 import { graphql } from 'react-apollo'
 
-const profileUpdate = async (e, id, updateProfile) => {
-  e.preventDefault()
+const wrapComponentWithState = provideState({
+  initialState: props => ({
+    inputValue: props.user.firstName
+  }),
+  effects: {
+    updateField: (effects, value) => state =>
+      Object.assign({}, state, { inputValue: value })
+  }
+})
+
+const profileUpdate = async (id, updateProfile, updateField) => {
   const firstName = document.getElementById('firstName').value
+  updateField(firstName)
   const lastName = document.getElementById('lastName').value
   const email = document.getElementById('email').value
-  await updateProfile(id, firstName, lastName, email)
+  const data = await updateProfile(id, firstName, lastName, email)
 }
 
-const ProfilePage = ({ user, updateProfile }) => {
-  return (
-    <form onSubmit={e => profileUpdate(e, user.id, updateProfile)}>
-      <fieldset>
-        <legend>My Profile</legend>
-        <div className="input-group fluid">
-          <label className="input-label" htmlFor="firstName">
-            First Name
-          </label>
-          <input
-            type="text"
-            id="firstName"
-            placeholder="first name"
-            defaultValue={user.firstName}
-            required
-          />
-        </div>
-        <div className="input-group fluid">
-          <label className="input-label" htmlFor="lastName">
-            Last Name
-          </label>
-          <input
-            type="text"
-            id="lastName"
-            placeholder="last name"
-            required
-            defaultValue={user.lastName}
-          />
-        </div>
-        <div className="input-group fluid">
-          <label className="input-label" htmlFor="username">
-            Username
-          </label>
-          <input
-            type="text"
-            id="username"
-            placeholder="username"
-            disabled
-            defaultValue={user.username}
-          />
-        </div>
-        <div className="input-group fluid">
-          <label className="input-label" htmlFor="email">
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            placeholder="email"
-            disabled
-            defaultValue={user.email}
-          />
-        </div>
-      </fieldset>
-      <style jsx>{`
+const ProfilePage = wrapComponentWithState(
+  injectState(({ state, effects, user, updateProfile }) => {
+    return (
+      <form>
+        <fieldset>
+          <legend>My Profile</legend>
+          <div className="input-group fluid">
+            <label className="input-label" htmlFor="firstName">
+              First Name
+            </label>
+            <input
+              type="text"
+              id="firstName"
+              placeholder="first name"
+              value={state.inputValue}
+              onChange={() =>
+                profileUpdate(user.id, updateProfile, effects.updateField)}
+              required
+            />
+          </div>
+          <div className="input-group fluid">
+            <label className="input-label" htmlFor="lastName">
+              Last Name
+            </label>
+            <input
+              type="text"
+              id="lastName"
+              placeholder="last name"
+              required
+              defaultValue={user.lastName}
+            />
+          </div>
+          <div className="input-group fluid">
+            <label className="input-label" htmlFor="username">
+              Username
+            </label>
+            <input
+              type="text"
+              id="username"
+              placeholder="username"
+              disabled
+              defaultValue={user.username}
+            />
+          </div>
+          <div className="input-group fluid">
+            <label className="input-label" htmlFor="email">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              placeholder="email"
+              disabled
+              defaultValue={user.email}
+            />
+          </div>
+        </fieldset>
+        <style jsx>{`
         .input-label {
           width: 100px;
         }
       `}</style>
-    </form>
-  )
-}
+      </form>
+    )
+  })
+)
 
 ProfilePage.propTypes = {
   user: PropTypes.object,
