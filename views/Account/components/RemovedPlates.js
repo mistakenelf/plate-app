3b62658105ab5adbb1aae971b5d7eb14a6ec7e90
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-05-17 23:23:25 -0500

    display proper message if there are no plates to be recovered

--- views/Account/components/RemovedPlates.js
+++ views/Account/components/RemovedPlates.js
@@ -24,44 +24,59 @@ const RemovedPlates = ({
   addRecoveredPlate,
   removeRecoveredPlate
 }) => {
-  return (
-    <table style={{ width: '100%', textAlign: 'center' }}>
-      <caption>Recover Plates</caption>
-      <thead>
-        <tr>
-          <th>Plate Name</th>
-          <th>Recover Plate</th>
-          <th>Remove Plate</th>
-        </tr>
-      </thead>
-      <tbody>
-        {recoverPlates.map((plate, index) => (
-          <tr key={index}>
-            <td data-label="name">{plate.name}</td>
-            <td data-label="recover">
-              <button
-                type="button"
-                className="primary"
-                onClick={() =>
-                  recoverPlate(plate, addRecoveredPlate, removeRecoveredPlate)}
-              >
-                Recover Plate
-              </button>
-            </td>
-            <td data-label="remove">
-              <button
-                type="button"
-                className="secondary"
-                onClick={() => removePlate(plate.id, removeRecoveredPlate)}
-              >
-                Remove Plate
-              </button>
-            </td>
+  if (recoverPlates.length === 0) {
+    return (
+      <div>
+        <h2 style={{ textAlign: 'center' }}>Recover Plates</h2>
+        <h3 style={{ textAlign: 'center' }}>
+          <i>You currently have no plates to be recovered.</i>
+        </h3>
+      </div>
+    )
+  } else {
+    return (
+      <table style={{ width: '100%', textAlign: 'center' }}>
+        <caption>Recover Plates</caption>
+        <thead>
+          <tr>
+            <th>Plate Name</th>
+            <th>Recover Plate</th>
+            <th>Remove Plate</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
-  )
+        </thead>
+        <tbody>
+          {recoverPlates.map((plate, index) => (
+            <tr key={index}>
+              <td data-label="name">{plate.name}</td>
+              <td data-label="recover">
+                <button
+                  type="button"
+                  className="primary"
+                  onClick={() =>
+                    recoverPlate(
+                      plate,
+                      addRecoveredPlate,
+                      removeRecoveredPlate
+                    )}
+                >
+                  Recover Plate
+                </button>
+              </td>
+              <td data-label="remove">
+                <button
+                  type="button"
+                  className="secondary"
+                  onClick={() => removePlate(plate.id, removeRecoveredPlate)}
+                >
+                  Remove Plate
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    )
+  }
 }
 
 RemovedPlates.propTypes = {
