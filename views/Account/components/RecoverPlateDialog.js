950fbbc80319793db61f02a1f9de74b51d89044d
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-05-18 21:24:21 -0500

    use modal for recover confirmation

---
+++ views/Account/components/RecoverPlateDialog.js
@@ -0,0 +1,66 @@
+import Modal from '../../../components/Modal/Modal'
+import PropTypes from 'prop-types'
+import React from 'react'
+
+const recoverPlate = async (
+  plate,
+  addRecoveredPlate,
+  removeRecoveredPlate,
+  closeDialog
+) => {
+  await addRecoveredPlate(
+    plate.name,
+    plate.description,
+    plate.thumbnail,
+    plate.status,
+    plate.content,
+    plate.createdBy
+  )
+  await removeRecoveredPlate(plate.id)
+  closeDialog()
+}
+
+const removePlate = async (plateId, removeRecoveredPlate) => {
+  await removeRecoveredPlate(plateId)
+}
+
+const RecoverPlateDialog = ({
+  dialogOpen,
+  closeDialog,
+  addRecoveredPlate,
+  removeRecoveredPlate,
+  plate
+}) => (
+  <Modal open={dialogOpen} closeModal={closeDialog}>
+    <h3 style={{ color: 'white', marginTop: 150 }}>
+      Are you sure you want to recover this plate?
+    </h3>
+    <button type="secondary" className="secondary" onClick={closeDialog}>
+      Cancel
+    </button>
+    <button
+      type="submit"
+      className="primary"
+      style={{ float: 'right' }}
+      onClick={() =>
+        recoverPlate(
+          plate,
+          addRecoveredPlate,
+          removeRecoveredPlate,
+          closeDialog
+        )}
+    >
+      Confirm
+    </button>
+  </Modal>
+)
+
+RecoverPlateDialog.propTypes = {
+  dialogOpen: PropTypes.bool,
+  closeDialog: PropTypes.func,
+  addRecoveredPlate: PropTypes.func,
+  removeRecoveredPlate: PropTypes.func,
+  plate: PropTypes.object
+}
+
+export default RecoverPlateDialog
--- views/Account/components/RemovedPlates.js
+++ views/Account/components/RemovedPlates.js
@@ -1,85 +1,96 @@
+import { injectState, provideState } from 'freactal'
+
 import PropTypes from 'prop-types'
 import React from 'react'
+import RecoverPlateDialog from './RecoverPlateDialog'
 
-const recoverPlate = async (plate, addRecoveredPlate, removeRecoveredPlate) => {
-  await addRecoveredPlate(
-    plate.name,
-    plate.description,
-    plate.thumbnail,
-    plate.status,
-    plate.content,
-    plate.createdBy
-  )
-  await removeRecoveredPlate(plate.id)
-}
-
-const removePlate = async (plateId, removeRecoveredPlate) => {
-  await removeRecoveredPlate(plateId)
-}
-
-const RemovedPlates = ({
-  recoverPlates,
-  addRecoveredPlate,
-  removeRecoveredPlate
-}) => {
-  if (recoverPlates.length === 0) {
-    return (
-      <div>
-        <h2 style={{ textAlign: 'center' }}>Recover Plates</h2>
-        <h3 style={{ textAlign: 'center' }}>
-          <i>You currently have no plates to be recovered.</i>
-        </h3>
-      </div>
-    )
-  } else {
-    return (
-      <table style={{ width: '100%', textAlign: 'center' }}>
-        <caption>Recover Plates</caption>
-        <thead>
-          <tr>
-            <th>Plate Name</th>
-            <th>Recover Plate</th>
-            <th>Remove Plate</th>
-            <th>Date Deleted</th>
-          </tr>
-        </thead>
-        <tbody>
-          {recoverPlates.map((plate, index) => (
-            <tr key={index}>
-              <td data-label="name">{plate.name}</td>
-              <td data-label="recover">
-                <button
-                  type="button"
-                  className="primary"
-                  onClick={() =>
-                    recoverPlate(
-                      plate,
-                      addRecoveredPlate,
-                      removeRecoveredPlate
-                    )}
-                >
-                  Recover Plate
-                </button>
-              </td>
-              <td data-label="remove">
-                <button
-                  type="button"
-                  className="secondary"
-                  onClick={() => removePlate(plate.id, removeRecoveredPlate)}
-                >
-                  Remove Plate
-                </button>
-              </td>
-              <td data-label="date-deleted">
-                {plate.dateDeleted}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    )
+const wrapComponentWithState = provideState({
+  initialState: () => ({
+    recoverDialogOpen: false
+  }),
+  effects: {
+    openRecoverDialog: () => state =>
+      Object.assign({}, state, { recoverDialogOpen: true }),
+    closeRecoverDialog: () => state =>
+      Object.assign({}, state, { recoverDialogOpen: false })
   }
-}
+})
+
+const RemovedPlates = wrapComponentWithState(
+  injectState(
+    ({
+      state,
+      effects,
+      recoverPlates,
+      addRecoveredPlate,
+      removeRecoveredPlate
+    }) => {
+      if (recoverPlates.length === 0) {
+        return (
+          <div>
+            <h2 style={{ textAlign: 'center' }}>Recover Plates</h2>
+            <h3 style={{ textAlign: 'center' }}>
+              <i>You currently have no plates to be recovered.</i>
+            </h3>
+          </div>
+        )
+      } else {
+        return (
+          <div>
+            <table style={{ width: '100%', textAlign: 'center' }}>
+              <caption>Recover Plates</caption>
+              <thead>
+                <tr>
+                  <th>Plate Name</th>
+                  <th>Recover Plate</th>
+                  <th>Remove Plate</th>
+                  <th>Date Deleted</th>
+                </tr>
+              </thead>
+              <tbody>
+                {recoverPlates.map((plate, index) => (
+                  <tr key={index}>
+                    <td data-label="name">{plate.name}</td>
+                    <td data-label="recover">
+                      <button
+                        type="button"
+                        className="primary"
+                        onClick={effects.openRecoverDialog}
+                      >
+                        Recover Plate
+                      </button>
+                      <RecoverPlateDialog
+                        dialogOpen={state.recoverDialogOpen}
+                        closeDialog={effects.closeRecoverDialog}
+                        addRecoveredPlate={addRecoveredPlate}
+                        removeRecoveredPlate={removeRecoveredPlate}
+                        plate={plate}
+                      />
+                    </td>
+                    <td data-label="remove">
+                      <button
+                        type="button"
+                        className="secondary"
+                        onClick={() =>
+                          removePlate(plate.id, removeRecoveredPlate)}
+                      >
+                        Remove Plate
+                      </button>
+                    </td>
+                    <td data-label="date-deleted">
+                      {plate.dateDeleted}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+
+          </div>
+        )
+      }
+    }
+  )
+)
 
 RemovedPlates.propTypes = {
   recoverPlates: PropTypes.array,
