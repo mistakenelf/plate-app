f20b8b716de58ee7721faf58f4a2a0c3ad7ec8ca
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-05-21 20:28:11 -0500

    new account page testing

--- pages/account.js
+++ pages/account.js
@@ -1,4 +1,4 @@
-import Account from '../views/Account'
+import Account from '../views/Account/indexTest'
 import React from 'react'
 import Unauthorized from '../components/Unauthorized/Unauthorized'
 import { pageWithUserData } from '../hocs/page'
