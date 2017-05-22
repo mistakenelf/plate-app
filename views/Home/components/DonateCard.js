c68785c30c781468b7bc485d4d5cb91a1b4bcc7f
knipferrc <knipferrc@gmail.com>
2017-05-22 13:03:32 -0500

    fix prop warnings
    Signed-off-by: knipferrc <knipferrc@gmail.com>

--- views/Home/components/DonateCard.js
+++ views/Home/components/DonateCard.js
@@ -24,16 +24,12 @@ const DonateCard = () => {
           <input
             type="image"
             src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif"
-            border="0"
             name="submit"
             alt="PayPal - The safer, easier way to pay online!"
           />
           <img
             alt=""
-            border="0"
             src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif"
-            width="1"
-            height="1"
           />
         </form>
       </div>
