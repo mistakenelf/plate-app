13fc64aa4e069b33b7af45c169b503dda1ccc66d
knipferrc <knipferrc@gmail.com>
2017-05-23 07:06:47 -0500

    remove react media
    Signed-off-by: knipferrc <knipferrc@gmail.com>

--- views/Account/indexTest.js
+++ views/Account/indexTest.js
@@ -4,7 +4,6 @@ import { compose, graphql } from 'react-apollo'
 import AddRecoveredPlateMutation from '../../mutations/addRecoveredPlate'
 import ChangePassword from './components/ChangePassword'
 import Loader from '../../components/Loader/Loader'
-import Media from 'react-media'
 import PlatesQuery from '../../queries/plates'
 import ProfilePage from './components/ProfilePage'
 import PropTypes from 'prop-types'
@@ -71,23 +70,17 @@ class Account extends Component {
             </span>
           </div>
         </div>
-        <Media query="(min-width: 599px)">
-          {matches =>
-            matches &&
-            <div>
-              {this.state.currentView === 'overview' &&
-                <ProfilePage user={user} />}
-              {this.state.currentView === 'security' &&
-                <ChangePassword user={user} />}
-              {this.state.currentView === 'tools' &&
-                <RemovedPlates
-                  recoverPlates={recoveredPlates}
-                  addRecoveredPlate={addRecoveredPlate}
-                  removeRecoveredPlate={removeRecoveredPlate}
-                />}
-            </div>}
-        </Media>
-
+        <div>
+          {this.state.currentView === 'overview' && <ProfilePage user={user} />}
+          {this.state.currentView === 'security' &&
+            <ChangePassword user={user} />}
+          {this.state.currentView === 'tools' &&
+            <RemovedPlates
+              recoverPlates={recoveredPlates}
+              addRecoveredPlate={addRecoveredPlate}
+              removeRecoveredPlate={removeRecoveredPlate}
+            />}
+        </div>
         <style jsx>{`
           @media only screen
           and (min-device-width : 320px)
@@ -99,81 +92,76 @@ class Account extends Component {
               margin-top: 100px;
             }
           }
-        .divider {
-          margin-top: 40px;
-          margin-bottom: 40px;
-        }
-        .bottom-padding {
-          padding-bottom: 30px;
-        }
-        .wrapper{
-          margin:3em 0;
-        }
-        .account-links {
-          display: flex;
-          justify-content: center;
-        }
-        .navigation-container {
-          display: flex;
-          justify-content: center;
-          margin-bottom: 60px;
-        }
-        .navigation a {
-          position: relative;
-          display: inline-block;
-          margin: 15px 25px;
-          outline: none;
-          color: #fff;
-          text-decoration: none;
-          text-transform: uppercase;
-          letter-spacing: 1px;
-          font-weight: 400;
-          text-shadow: 0 0 1px rgba(255,255,255,0.3);
-          font-size: 1.35em;
-        }
-
-        .navigation a:hover,
-        .navigation a:focus {
-          outline: none;
-        }
-        .nav-link a {
-          padding: 12px 10px 10px;
-          color: #566473;
-          text-shadow: none;
-          font-weight: 700;
-        }
-
-        .nav-link a::before,
-        .nav-link a::after {
-          position: absolute;
-          top: 100%;
-          left: 0;
-          width: 100%;
-          height: 3px;
-          background: #566473;
-          content: '';
-          transition: transform 0.3s;
-          transform: scale(0.85);
-        }
-
-        .nav-link a::after {
-          opacity: 0;
-          transition: top 0.3s, opacity 0.3s, transform 0.3s;
-        }
-
-        .nav-link a:hover::before,
-        .nav-link a:hover::after,
-        .nav-link a:focus::before,
-        .nav-link a:focus::after {
-          transform: scale(1);
-        }
-
-        .nav-link a:hover::after,
-        .nav-link a:focus::after {
-          top: 0%;
-          opacity: 1;
-        }
-      `}</style>
+          .divider {
+            margin-top: 40px;
+            margin-bottom: 40px;
+          }
+          .bottom-padding {
+            padding-bottom: 30px;
+          }
+          .wrapper{
+            margin:3em 0;
+          }
+          .account-links {
+            display: flex;
+            justify-content: center;
+          }
+          .navigation-container {
+            display: flex;
+            justify-content: center;
+            margin-bottom: 60px;
+          }
+          .navigation a {
+            position: relative;
+            display: inline-block;
+            margin: 15px 25px;
+            outline: none;
+            color: #fff;
+            text-decoration: none;
+            text-transform: uppercase;
+            letter-spacing: 1px;
+            font-weight: 400;
+            text-shadow: 0 0 1px rgba(255,255,255,0.3);
+            font-size: 1.35em;
+          }
+          .navigation a:hover,
+          .navigation a:focus {
+            outline: none;
+          }
+          .nav-link a {
+            padding: 12px 10px 10px;
+            color: #566473;
+            text-shadow: none;
+            font-weight: 700;
+          }
+          .nav-link a::before,
+          .nav-link a::after {
+            position: absolute;
+            top: 100%;
+            left: 0;
+            width: 100%;
+            height: 3px;
+            background: #566473;
+            content: '';
+            transition: transform 0.3s;
+            transform: scale(0.85);
+          }
+          .nav-link a::after {
+            opacity: 0;
+            transition: top 0.3s, opacity 0.3s, transform 0.3s;
+          }
+          .nav-link a:hover::before,
+          .nav-link a:hover::after,
+          .nav-link a:focus::before,
+          .nav-link a:focus::after {
+            transform: scale(1);
+          }
+          .nav-link a:hover::after,
+          .nav-link a:focus::after {
+            top: 0%;
+            opacity: 1;
+          }
+        `}</style>
       </div>
     )
   }
