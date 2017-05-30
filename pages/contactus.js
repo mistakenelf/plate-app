3fc45a0b90c78e2b5455f1618fd19b44ad7f3794
knipferrc <knipferrc@gmail.com>
2017-05-30 07:01:15 -0500

    fixes

--- pages/contactus.js
+++
@@ -1,5 +0,0 @@
-import ContactUs from '../views/ContactUs'
-import React from 'react'
-import { pageWithDefaultLayout } from '../hocs/page'
-
-export default pageWithDefaultLayout(() => <ContactUs />)
