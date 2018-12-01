1ecd4681dba7bc5304da6dd1b4648d2317db9fcb
alexknipfer <alexanderknipfer@gmail.com>
2018-11-30 22:53:59 -0600

    Update

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-docker login $DOCKER_REGISTRY -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-
-docker build -f Dockerfile -t plate-dev
-docker tag plate-dev: $DOCKER_REGISTRY/plate-dev:
-docker push $DOCKER_REGISTRY/plate-dev
