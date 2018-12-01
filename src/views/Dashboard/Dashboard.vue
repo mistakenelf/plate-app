8c756ad00f4132012f18aa8880beadbc153c82af
knipferrc <knipferrc@gmail.com>
2018-12-01 15:43:49 -0500

    format

--- src/views/Dashboard/Dashboard.vue
+++ src/views/Dashboard/Dashboard.vue
@@ -1,22 +1,20 @@
 <template>
   <AppLayout>
-    <ApolloQuery
-  :query="require('./queries/hello.gql')"
->
-  <template slot-scope="{ result: { loading, error, data } }">
-    <!-- Loading -->
-    <div v-if="loading" class="loading apollo">Loading...</div>
+    <ApolloQuery :query="require('./queries/hello.gql')">
+      <template slot-scope="{ result: { loading, error, data } }">
+        <!-- Loading -->
+        <div v-if="loading" class="loading apollo">Loading...</div>
 
-    <!-- Error -->
-    <div v-else-if="error" class="error apollo">An error occured</div>
+        <!-- Error -->
+        <div v-else-if="error" class="error apollo">An error occured</div>
 
-    <!-- Result -->
-    <div v-else-if="data" class="result apollo">{{ data.hello }}</div>
+        <!-- Result -->
+        <div v-else-if="data" class="result apollo">{{ data.hello }}</div>
 
-    <!-- No result -->
-    <div v-else class="no-result apollo">No result :(</div>
-  </template>
-</ApolloQuery>
+        <!-- No result -->
+        <div v-else class="no-result apollo">No result :(</div>
+      </template>
+    </ApolloQuery>
   </AppLayout>
 </template>
 
