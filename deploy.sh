46da2d94c39da4dd5135c2b21845eac5513510e6
knipferrc <knipferrc@gmail.com>
2018-12-08 16:33:57 -0500

    try again

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
+echo "$REGISTRY_PASS" | docker login -u "$REGISTRY_USER" --password-stdin "$REGISTRY"
+
 docker-compose build
-echo $REGISTRY_PASS | docker login -u $REGISTRY_USER --password-stdin $REGISTRY
+
 docker tag plate_app $REGISTRY/plate_app
 docker tag plate_app $REGISTRY/plate_api
 docker push $REGISTRY/plate_app
