0a3244c921533ddb780139b77e411e3d8c1cf975
knipferrc <knipferrc@gmail.com>
2018-12-08 15:57:07 -0500

    test deploy

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sh docker-compose build
+echo "$REGISTRY_PASS" | docker login -u "$REGISTRY_USER" --password-stdin "$REGISTRY"
+docker tag plate_app "${REGISTRY}/plate_app"
+docker tag plate_app "${REGISTRY}/plate_api"
+docker push "${REGISTRY}/plate_app"
+docker push "${REGISTRY}/plate_api"
