f192cbb1c1788d40764e0a1513e1a4a43cb5238a
knipferrc <knipferrc@gmail.com>
2018-12-09 12:50:15 -0500

    fix cors

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,7 +5,6 @@ export const API = axios.create({
     process.env.NODE_ENV === 'production'
       ? 'https://plate-api.azurewebsites.net'
       : 'http://localhost:5000',
-  withCredentials: true,
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json'
