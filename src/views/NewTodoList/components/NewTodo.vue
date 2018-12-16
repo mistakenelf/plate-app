ca779fcd78975ad2c43adba8466887b5fae01849
knipferrc <knipferrc@gmail.com>
2018-12-16 10:15:55 -0500

    WIP add todo

--- src/views/NewTodoList/components/NewTodo.vue
+++ src/views/NewTodoList/components/NewTodo.vue
@@ -1,8 +1,41 @@
 <template>
-  <div
-    class="cursor-pointer p-4 border-4 border-dashed border-2 border-grey mt-4 flex justify-center flex-col items-center w-full sm:w-full md:w-1/4 lg:w-1/5 rounded"
-  >
-    <font-awesome-icon icon="plus" class="text-5xl mb-4" />
-    <div class="text-xl">Add New todo</div>
+  <div class="flex flex-row flex-wrap">
+    <div
+      class="cursor-pointer p-2 m-2 mt-6 border-4 h-32 border-dashed border-grey flex justify-center flex-col items-center rounded w-full sm:w-full md:w-1/4 lg:w-1/5"
+      @click="addTodo()"
+    >
+      <font-awesome-icon icon="plus" class="text-5xl mb-4" />
+      <div class="text-xl">Add New todo</div>
+    </div>
+    <div
+      v-for="todo in todos"
+      :key="todo.id"
+      class="flex flex-row flex-wrap mt-4 w-full sm:w-full md:w-1/4 lg:w-1/5 flex-grow"
+    >
+      <div
+        class="bg-white shadow p-2 m-2 h-32 flex justify-center flex-col items-center rounded w-full"
+      >
+        <div>{{ todo.title }}</div>
+        <div>{{ todo.description }}</div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    todos: [],
+    count: 0
+  }),
+  methods: {
+    addTodo() {
+      this.todos.push({
+        title: 'New Todo',
+        description: 'Todo Description',
+        id: this.count++
+      })
+    }
+  }
+}
+</script>
