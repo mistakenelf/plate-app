c10621ed72003ebfb658c12634161e54493afadb
alexknipfer <alexanderknipfer@gmail.com>
2019-01-01 16:15:09 -0600

    Cleanup

--- src/modules/theme/_store/mutations.js
+++ src/modules/theme/_store/mutations.js
@@ -2,7 +2,6 @@ import { lightTheme, darkTheme } from '../../../helpers/theme'
 
 export default {
   TOGGLE_THEME_SUCCESS(state, themeName) {
-    console.log('THEME: ', themeName)
     ;(state.selectedThemeName = themeName),
       (state.selectedThemeClasses =
         themeName === 'light' ? lightTheme : darkTheme)
