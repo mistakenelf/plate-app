febc60d2b299e7b5c1afb96c25b473216fe0c069
alexknipfer <alexanderknipfer@gmail.com>
2019-01-01 16:12:53 -0600

    Add light and dark theme

---
+++ src/helpers/theme.js
@@ -0,0 +1,29 @@
+export const darkTheme = {
+  // Background colors
+  sideNavBg: 'bg-black',
+  totalsPanelBg: 'bg-black',
+  panelHeaderBg: 'bg-black',
+  todoListPanelBg: 'bg-grey-darker',
+  remindersPanelBg: 'bg-grey-darker',
+  notesPanelBg: 'bg-grey-darker',
+  eventsPanelBg: 'bg-grey-darker',
+  navbarBg: 'bg-black',
+
+  // General colors
+  textColor: 'text-white'
+}
+
+export const lightTheme = {
+  // Background colors
+  sideNavBg: 'bg-grey-lighter',
+  totalsPanelBg: 'bg-white',
+  panelHeaderBg: 'bg-teal-dark',
+  todoListPanelBg: 'bg-white',
+  remindersPanelBg: 'bg-white',
+  notesPanelBg: 'bg-white',
+  eventsPanelBg: 'bg-white',
+  navbarBg: 'bg-blue-darker',
+
+  // General colors
+  textColor: 'text-grey-darkest'
+}
