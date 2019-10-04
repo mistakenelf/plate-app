94c6d03bc5da551101acb11d26713750013af832
knipferrc <knipferrc@gmail.com>
2019-10-03 20:51:00 -0400

    add button component

---
+++ src/components/Button.svelte
@@ -0,0 +1,45 @@
+<script>
+  export let type = 'button'
+  export let fullWidth = false
+  export let variant = 'primary'
+</script>
+
+<style>
+  button {
+    border: none;
+    font-size: 16px;
+    font-weight: 700;
+    padding: 10px;
+    text-transform: uppercase;
+    border-radius: 3px;
+    cursor: pointer;
+  }
+
+  .full-width {
+    width: 100%;
+  }
+
+  .primary {
+    background: var(--primary);
+    color: var(--white);
+  }
+
+  .secondary {
+    background: var(--secondary);
+    color: var(--white);
+  }
+
+  .tertiary {
+    background: var(--tertiary);
+    color: var(--white);
+  }
+</style>
+
+<button
+  class:primary={variant === 'primary'}
+  class:secondary={variant === 'secondary'}
+  class:tertiary={variant === 'tertiary'}
+  class:full-width={fullWidth}
+  {type}>
+  <slot />
+</button>
