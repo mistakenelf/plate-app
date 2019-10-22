e2be730c8090e43cbe78091d352bdaf857247f1d
knipferrc <knipferrc@gmail.com>
2019-10-22 19:45:59 -0400

    add storage bucket

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -9,6 +9,7 @@ const app = firebase.initializeApp({
   databaseURL: process.env.DATABASE_URL,
   projectId: process.env.PROJECT_ID,
   messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
   appId: process.env.APP_ID
 })
 
