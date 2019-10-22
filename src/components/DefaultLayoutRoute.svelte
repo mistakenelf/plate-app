3bf97016009b2ad1af01809324663b5f061e5a06
knipferrc <knipferrc@gmail.com>
2019-10-22 19:46:11 -0400

    redirect on protected routes

--- src/components/DefaultLayoutRoute.svelte
+++ src/components/DefaultLayoutRoute.svelte
@@ -1,10 +1,19 @@
 <script>
-  import { Route } from 'svelte-routing'
+  import { onMount } from 'svelte'
+  import { Route, navigate } from 'svelte-routing'
 
+  import { currentUser } from '../store/auth'
   import DefaultLayout from '../layouts/DefaultLayout'
 
   export let path
   export let component
+  export let isProtected
+
+  onMount(() => {
+    if (isProtected & $currentUser && !$currentUser.uid) {
+      navigate('/login')
+    }
+  })
 </script>
 
 <Route {path}>
