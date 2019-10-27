fc11f2fdaae7f18d59a3e0b901a3e140dd2d0a00
knipferrc <knipferrc@gmail.com>
2019-10-27 16:26:29 -0400

    revert

--- webpack.config.js
+++ webpack.config.js
@@ -12,23 +12,8 @@ const mode = process.env.NODE_ENV || 'development'
 
 const prod = mode === 'production'
 
-const supportedBrowsers = [
-  '>1%',
-  'last 4 versions',
-  'Firefox ESR',
-  'not ie < 9'
-];
-
-const babelLoader = {
-  loader: 'babel-loader',
-  options: {
-    presets: [
-      ['@babel/preset-env', { targets: supportedBrowsers }]
-    ]
-  }
-};
-
 module.exports = {
+  devtool: prod ? false : 'source-map',
   entry: {
     bundle: ['./src/main.js']
   },
@@ -36,13 +21,6 @@ module.exports = {
     filename: '[name].js',
     chunkFilename: '[name].[id].js',
     publicPath: '/'
-  },
-  resolve: {
-    alias: {
-      svelte: path.resolve('node_modules', 'svelte')
-    },
-    extensions: ['.mjs', '.js', '.svelte'],
-    mainFields: ['svelte', 'browser', 'module', 'main']
   },
   devServer: {
     historyApiFallback: true,
@@ -55,51 +33,36 @@ module.exports = {
       errors: true
     }
   },
-  devtool: prod ? false : 'source-map',
   optimization: {
-    runtimeChunk: { name: 'runtime' },
     splitChunks: {
-      chunks: 'all',
-      minSize: 0,
-      minChunks: 1,
-      automaticNameDelimiter: '_',
       cacheGroups: {
-        vendors: false,
-        libs: {
-          test: /[\\/]node_modules[\\/]/,
-          priority: -10
-        },
-        polyfills: {
-          test: /core-js/,
-          name: 'polyfills',
-          priority: 10
+        vendor: {
+          test: /node_modules/,
+          chunks: 'initial',
+          name: 'vendor',
+          enforce: true
         }
       }
     }
   },
+  resolve: {
+    alias: {
+      svelte: path.resolve('node_modules', 'svelte')
+    },
+    extensions: ['.mjs', '.js', '.svelte'],
+    mainFields: ['svelte', 'browser', 'module', 'main']
+  },
   module: {
     rules: [
       {
-        oneOf: [
-          {
-            test: /\.(html|svelte)$/,
-            use: [
-              babelLoader,
-              {
-                loader: 'svelte-loader',
-                options: {
-                  emitCss: true,
-                  legacy: true
-                }
-              }
-            ]
-          },
-        ]
-      },
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules\/(?!svelte)/,
-        use: babelLoader
+        test: /\.svelte$/,
+        use: {
+          loader: 'svelte-loader',
+          options: {
+            emitCss: true,
+            hotReload: true
+          }
+        }
       },
       {
         test: /\.css$/,
@@ -125,22 +88,11 @@ module.exports = {
   },
   mode,
   plugins: [
-    prod && new CleanWebpackPlugin(),
+    new CleanWebpackPlugin(),
     new HtmlWebPackPlugin({
       template: './public/index.html',
       filename: './index.html',
-      minify: !prod ? undefined : {
-        removeComments: true,
-        collapseWhitespace: true,
-        removeRedundantAttributes: true,
-        useShortDoctype: true,
-        removeEmptyAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        keepClosingSlash: true,
-        minifyJS: true,
-        minifyCSS: true,
-        minifyURLs: true,
-      },
+      minify: process.env.NODE_ENV === 'production',
       favicon: path.join(__dirname, './public/favicon.png')
     }),
     new CompressionPlugin(),
@@ -152,5 +104,5 @@ module.exports = {
       safe: false
     }),
     new webpack.EnvironmentPlugin(Object.keys(env.parsed))
-  ].filter(Boolean)
+  ]
 }
