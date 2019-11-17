0b8c1bbdd72cf53e43e7099240c02d5c4e675ada
knipferrc <knipferrc@gmail.com>
2019-11-17 11:21:19 -0500

    see if this fixing routing

--- src/sw.js
+++ src/sw.js
@@ -1,6 +1,8 @@
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
 
-workbox.routing.registerNavigationRoute('/index.html');
+workbox.routing.registerNavigationRoute(
+  workbox.precaching.getCacheKeyForURL('/index.html')
+);
 
 addEventListener('message', event => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
