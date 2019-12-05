fdd033e3341a4ba663895b2c9c7fb43002445b78
knipferrc <knipferrc@gmail.com>
2019-12-04 20:34:20 -0500

    add functions

--- src/routes/Dashboard/index.svelte
+++ src/routes/Dashboard/index.svelte
@@ -1,8 +1,15 @@
 <script>
   import { faPlus } from '@fortawesome/free-solid-svg-icons/faPlus';
   import page from 'page';
+  import { onMount } from 'svelte'
 
   import FAB from '../../components/FAB';
+  let json
+
+  onMount(async () => {
+    const data = await fetch("/.netlify/functions/hello")
+    json = await data.json()
+  })
 </script>
 
 <svelte:head>
@@ -10,5 +17,6 @@
 </svelte:head>
 
 <div>Dashboard</div>
+<div>{json}</div>>
 
 <FAB icon={faPlus} on:click={() => page('/create-plate')} />
