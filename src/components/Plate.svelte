bcae3a2c67b42ddb94b6139be06c072d02e098a6
knipferrc <knipferrc@gmail.com>
2019-12-18 18:19:57 -0500

    add back status

--- src/components/Plate.svelte
+++ src/components/Plate.svelte
@@ -2,16 +2,19 @@
   import page from 'page';
   import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons/faExternalLinkAlt';
 
+  import StatusChip from './StatusChip';
   import Icon from './Icon';
 
   export let name;
   export let id;
+  export let status;
 </script>
 
 <div class="shadow p-3 bg-white mb-4">
   <div class="flex items-center justify-between">
     <div class="text-md md:text-xl w-1/2 truncate">{name}</div>
     <div class="items-center flex justify-center">
+      <StatusChip status={status} />
       <Icon
         fill="#4a5568"
         icon={faExternalLinkAlt}
