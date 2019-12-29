5d9a741fea63f6462af8feb66a12f8bac00898f2
knipferrc <knipferrc@gmail.com>
2019-12-29 16:14:30 -0500

    format

--- api/get-plates.js
+++ api/get-plates.js
@@ -7,14 +7,7 @@ const client = new faunadb.Client({ secret });
 module.exports = async (req, res) => {
   try {
     const dbs = await client.query(
-      q.Map(
-        q.Paginate(
-          q.Match(
-            q.Index('all_plates'),
-          ),
-        ),
-        ref => q.Get(ref),
-      ),
+      q.Map(q.Paginate(q.Match(q.Index('all_plates'))), ref => q.Get(ref)),
     );
     res.status(200).json(dbs.data);
   } catch (e) {
