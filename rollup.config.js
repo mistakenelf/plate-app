7843dafa7086c7c18ff145145c9f3e7288502561
knipferrc <knipferrc@gmail.com>
2019-12-29 18:01:30 -0500

    eslint plugin rollup

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import livereload from 'rollup-plugin-livereload';
 import resolve from '@rollup/plugin-node-resolve';
 import svelte from 'rollup-plugin-svelte';
 import sveltePreprocess from 'svelte-preprocess';
+import { eslint } from 'rollup-plugin-eslint'
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -37,6 +38,7 @@ export default {
     file: 'public/build/bundle.js',
   },
   plugins: [
+    eslint(),
     svelte({
       dev: !production,
       preprocess: sveltePreprocess({ postcss: true }),
@@ -52,7 +54,7 @@ export default {
     }),
     commonjs(),
     !production && serve(),
-    !production && livereload({ watch: 'public', port: 5000 }),
+    !production && livereload({ watch: 'public', port: 8080 }),
     production && terser(),
   ],
   watch: {
