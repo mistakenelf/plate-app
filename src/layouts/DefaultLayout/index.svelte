2792a4c0fe5e16ef535aa1235e283b169373c170
knipferrc <knipferrc@gmail.com>
2020-01-04 11:21:33 -0500

    fix index

--- src/layouts/DefaultLayout/index.svelte
+++ src/layouts/DefaultLayout/index.svelte
@@ -21,34 +21,32 @@
 </script>
 
 <main>
-  <div class="fixed top-0 w-full">
-    <nav
-      class="flex items-center w-full justify-between h-12 fixed z-10 left-0
-      bg-blue-500">
-      <Brand />
-      <div class="hidden lg:flex items-center h-full">
-        <Navbar />
+  <nav
+    class="flex items-center w-full justify-between h-12 fixed z-10 left-0 top-0
+    bg-blue-500">
+    <Brand />
+    <div class="hidden lg:flex items-center h-full">
+      <Navbar />
+    </div>
+    <div class="lg:hidden flex items-center h-full pr-4">
+      <div
+        bind:this={triggerEl}
+        class="ml-4 cursor-pointer"
+        aria-label="main menu"
+        role="button"
+        on:click={() => toggleMenu()}>
+        <Icon width="1.5em" height="1.5em" fill="#fff" icon={faBars} />
       </div>
-      <div class="lg:hidden flex items-center h-full pr-4">
-        <div
-          bind:this={triggerEl}
-          class="ml-4 cursor-pointer"
-          aria-label="main menu"
-          role="button"
-          on:click={() => toggleMenu()}>
-          <Icon width="1.5em" height="1.5em" fill="#fff" icon={faBars} />
-        </div>
-      </div>
-      {#if menuOpen}
-        <ClickOutside
-          class="absolute w-full top-0"
-          on:clickoutside={closeMenu}
-          exclude={[triggerEl]}>
-          <MobileMenu on:closeMenu={closeMenu} />
-        </ClickOutside>
-      {/if}
-    </nav>
-  </div>
+    </div>
+    {#if menuOpen}
+      <ClickOutside
+        class="absolute w-full top-0"
+        on:clickoutside={closeMenu}
+        exclude={[triggerEl]}>
+        <MobileMenu on:closeMenu={closeMenu} />
+      </ClickOutside>
+    {/if}
+  </nav>
   <div class="pt-12 h-full">
     <slot />
   </div>
