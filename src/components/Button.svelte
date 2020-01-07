d8d2adf5e581185e15a4a0e14d01a7d650cd99fc
knipferrc <knipferrc@gmail.com>
2020-01-06 20:22:15 -0500

    fix loading

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -8,19 +8,14 @@
 
 <style>
   .spinner {
-    position: relative;
-    color: transparent !important;
     pointer-events: none;
   }
 
   .spinner::after {
     content: '';
-    position: absolute !important;
-    top: calc(50% - (1em / 2));
-    left: calc(50% - (1em / 2));
     display: block;
-    width: 1em;
-    height: 1em;
+    width: 1.2em;
+    height: 1.2em;
     border: 2px solid #fff;
     border-radius: 9999px;
     border-right-color: transparent;
@@ -45,7 +40,7 @@
   class:w-full={fullWidth}
   {type}>
   {#if loading}
-    <div class="spinner px-4 md:px-5 xl:px-4 py-3" />
+    <div class="spinner" />
   {:else}
     <slot />
   {/if}
