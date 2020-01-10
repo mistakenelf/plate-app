9f8176f875e44504e842a0454bdd351bdb5f56c6
ploeffler <philiploeffler@gmail.com>
2020-01-10 15:35:14 -0500

    added hover affect to cards

--- src/routes/Dashboard/components/StatCard.svelte
+++ src/routes/Dashboard/components/StatCard.svelte
@@ -4,10 +4,16 @@ export let statData
 </script>
 
 
+<style>
+  .StatCard:hover {
+    transition: 0.2s ease-in-out;
+    transform: scale(1.01);
+    }
+</style>
 
 <!-- <statCard status="completed" count={completedCount} />
 <statCard status="open" count={openCount} /> -->
-<div class="max-w-sm rounded-lg m-10 flex-1 overflow-hidden shadow-lg">
+<div class= " StatCard max-w-sm rounded-lg m-10 flex-1 overflow-hidden shadow-lg">
   <div class="px-6 py-4">
     <div class="font-bold text-xl mb-2">{statData.text} </div>
      <div class="text-6xl">{statData.count} </div>
