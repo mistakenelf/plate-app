f54d23939982e51ed15a2b0868bd7c1cc9fe3039
knipferrc <knipferrc@gmail.com>
2020-01-21 21:09:03 -0500

    translate login

--- src/routes/Login/index.svelte
+++ src/routes/Login/index.svelte
@@ -1,5 +1,6 @@
 <script>
   import page from 'page';
+  import { _ } from 'svelte-i18n';
 
   import authStore, { errorMessage } from '../../store/auth';
   import TextField from '../../components/TextField.svelte';
@@ -34,22 +35,22 @@
     on:submit|preventDefault={handleSubmit}
     class="bg-white shadow-lg rounded px-8 pt-6 pb-8 mb-4 w-full md:w-3/4
     lg:w-1/2 xl:w-1/3">
-    <h1 class="text-3xl text-gray-800 mb-4 font-bold">Login</h1>
+    <h1 class="text-3xl text-gray-800 mb-4 font-bold">{$_('login.title')}</h1>
     <TextField
       name="username"
-      label="Username"
+      label={$_('login.username')}
       isFormField
       required
-      placeholder="username"
+      placeholder={$_('login.username')}
       bind:textFieldValue={formValues.username} />
     <PasswordField
       name="password"
-      label="Password"
+      label={$_('login.password')}
       isFormField
       required
-      placeholder="password"
+      placeholder={$_('login.password')}
       bind:passwordFieldValue={formValues.password} />
-    <Button type="submit" fullWidth loading={submitting}>Login</Button>
+    <Button type="submit" fullWidth loading={submitting}>{$_('login.submitText')}</Button>
     {#if $errorMessage}
       <div class="mt-4 bg-red-500 rounded p-2 font-bold text-white text-xl">
         {$errorMessage}
