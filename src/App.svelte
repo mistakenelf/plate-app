fbca13d1b3852ccd483a8168f88580cbfcb7fbaa
knipferrc <knipferrc@gmail.com>
2020-02-29 23:57:34 -0500

    test service worker

--- src/App.svelte
+++ src/App.svelte
@@ -20,6 +20,17 @@
 
   onMount(() => {
     firebase.initializeApp(firebaseConfig);
+
+    if (process.env.NODE_ENV === 'production') {
+      if ('serviceWorker' in navigator) {
+        window.addEventListener('load', () => {
+          navigator.serviceWorker
+            .register('/sw.js')
+            .then()
+            .catch();
+        });
+      }
+    }
   });
 </script>
 
---
+++ workbox-config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  globDirectory: 'dist/',
+  globPatterns: ['**/*.{js,png,css,html,json}'],
+  swDest: 'dist/sw.js',
+};
