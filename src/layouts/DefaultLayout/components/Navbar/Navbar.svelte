a43673a5811169def1539d69c8f4df486661c68c
knipferrc <knipferrc@gmail.com>
2020-03-09 08:15:43 -0400

    WIP mobile menu

--- src/layouts/DefaultLayout/components/Navbar/Navbar.svelte
+++ src/layouts/DefaultLayout/components/Navbar/Navbar.svelte
@@ -1,8 +1,11 @@
 <script>
   import firebase from 'firebase/app';
+  import { slide } from 'svelte/transition';
   import page from 'page';
 
   import { user } from '../../../../store/user';
+
+  let menuOpen = false;
 
   const logout = async () => {
     await firebase.auth().signOut();
@@ -11,23 +14,45 @@
   };
 </script>
 
+<style>
+  .mobile-menu {
+    top: 49px;
+  }
+</style>
+
 <nav
   class="fixed top-0 w-full flex bg-gray-800 justify-between pr-4 pl-4 h-12
   items-center shadow">
   <div class="flex items-center">
-    <svg
-      class="cursor-pointer md:hidden"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg">
-      <path
-        d="M4 6H20M4 12H20M4 18H11"
-        stroke="#fff"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round" />
-    </svg>
+    {#if menuOpen}
+      <svg
+        viewBox="0 0 20 20"
+        fill="#fff"
+        class="cursor-pointer w-6 h-6"
+        on:click={() => (menuOpen = false)}>
+        <path
+          fill-rule="evenodd"
+          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414
+          1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293
+          4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+          clip-rule="evenodd" />
+      </svg>
+    {:else}
+      <svg
+        on:click={() => (menuOpen = true)}
+        class="cursor-pointer md:hidden"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M4 6H20M4 12H20M4 18H11"
+          stroke="#fff"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round" />
+      </svg>
+    {/if}
     <div class="text-white text-3xl font-bold ml-2 md:ml-0">Plate</div>
   </div>
   <ul class="hidden md:flex flex-row">
@@ -58,4 +83,28 @@
         clip-rule="evenodd" />
     </svg>
   </div>
+  {#if menuOpen}
+    <div
+      class="bg-white w-full absolute mobile-menu left-0 p-4"
+      transition:slide>
+      <ul>
+        {#if $user}
+          <li class="font-bold mb-4">
+            <a href="/">Dashboard</a>
+          </li>
+          <li class="font-bold">
+            <div class="cursor-pointer" on:click={logout}>Logout</div>
+          </li>
+        {/if}
+        {#if !$user}
+          <li class="font-bold mb-4">
+            <a href="/login">Login</a>
+          </li>
+          <li class="font-bold">
+            <a href="/register">Register</a>
+          </li>
+        {/if}
+      </ul>
+    </div>
+  {/if}
 </nav>
