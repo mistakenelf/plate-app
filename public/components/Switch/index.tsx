0feaa82f0827fae3afa108fdc2e54d537c88cc31
knipferrc <knipferrc@gmail.com>
2020-12-23 09:31:46 -0500

    temp toggle button

--- public/components/Switch/index.tsx
+++ public/components/Switch/index.tsx
@@ -6,31 +6,19 @@ import styles from './style.module.css';
 const cx = classnames.bind(styles);
 
 interface SwitchProps {
-  id: string;
   isOn: boolean;
   handleToggle: () => void;
 }
 
 export const Switch: FunctionComponent<SwitchProps> = ({
-  id,
   isOn,
   handleToggle,
 }) => {
   return (
-    <>
-      <input
-        className={cx('switch-checkbox')}
-        id={id}
-        type="checkbox"
-        checked={isOn}
-        onClick={handleToggle}
-      />
-      <label
-        className={cx(['switch-label', { activated: isOn, inactive: !isOn }])}
-        htmlFor={id}
-      >
-        <span className={cx('switch-button')} />
-      </label>
-    </>
+    <button
+      type="button"
+      onClick={handleToggle}
+      class={cx('toggle', { activated: isOn, inactive: !isOn })}
+    />
   );
 };
