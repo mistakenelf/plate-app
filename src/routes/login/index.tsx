98adfb87fc51d86898769a13c03ec2248cca787c
knipferrc <knipferrc@gmail.com>
2020-12-30 12:40:11 -0500

    add back arrow

--- src/routes/login/index.tsx
+++ src/routes/login/index.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import classnames from 'classnames/bind';
 import * as yup from 'yup';
 import { useFormik } from 'formik';
+import feather from 'feather-icons';
+import { Link } from 'react-router-dom';
 
 import { supabase } from '../../helpers/supabase';
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
+import { useTheme } from '../../hooks/useTheme';
 
 import styles from './style.module.css';
 
@@ -20,6 +23,7 @@ const validationSchema = yup.object().shape({
 });
 
 const Login: React.FC = () => {
+  const { theme } = useTheme();
   const { values, handleChange, handleSubmit } = useFormik({
     validationSchema,
     initialErrors: {
@@ -44,23 +48,38 @@ const Login: React.FC = () => {
   });
 
   return (
-    <div className={cx('container')}>
-      <form className={cx('login-form')} onSubmit={handleSubmit}>
-        <Input
-          inputLabel="Email"
-          type="email"
-          placeholder="Email address"
-          id="email"
-          name="email"
-          value={values.email}
-          onChange={handleChange}
+    <>
+      <Link to="/">
+        <div
+          className={cx('back-arrow')}
+          dangerouslySetInnerHTML={{
+            __html: feather.icons['arrow-left'].toSvg({
+              'stroke-width': 2,
+              color: theme === 'dark' ? '#fff' : '#333',
+              height: 30,
+              width: 30,
+            }),
+          }}
         />
-        <div className={cx('separator')} />
-        <Button type="submit" block>
-          Login
-        </Button>
-      </form>
-    </div>
+      </Link>
+      <div className={cx('container')}>
+        <form className={cx('login-form')} onSubmit={handleSubmit}>
+          <Input
+            inputLabel="Email"
+            type="email"
+            placeholder="Email address"
+            id="email"
+            name="email"
+            value={values.email}
+            onChange={handleChange}
+          />
+          <div className={cx('separator')} />
+          <Button type="submit" block>
+            Login
+          </Button>
+        </form>
+      </div>
+    </>
   );
 };
 
