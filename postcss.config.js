a80bd1bd7bccaa3e9c649ad764bb32b918824961
knipferrc <knipferrc@gmail.com>
2021-01-03 09:41:22 -0500

    revert

--- postcss.config.js
+++ postcss.config.js
@@ -4,5 +4,5 @@ const cssnano = require('cssnano');
 const atImport = require('postcss-import');
 
 module.exports = {
-  plugins: [require('autoprefixer')],
+  plugins: [atImport(), cssnano(), autoprefixer(), postcssPresetEnv()],
 };
